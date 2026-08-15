6e9f1beb192365715c2a4167ecd9af138f97f097
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 00:39:59 +0200

    Add: MongoDB (Native Driver) - Node.js library docs

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 36
-// Last updated: 2026-08-16 00:39:56
+// Total modules: 37
+// Last updated: 2026-08-16 00:39:59
 
 module.exports = [
   {
@@ -435,5 +435,17 @@ module.exports = [
       "migrations"
     ],
     "category": "ORM/Database"
+  },
+  {
+    "title": "MongoDB (Native Driver)",
+    "description": "Official MongoDB driver with connection pooling, change streams, and aggregation pipeline support",
+    "usage": "const { MongoClient } = require('mongodb'); const client = new MongoClient('mongodb://localhost:27017'); await client.connect();",
+    "tags": [
+      "mongodb",
+      "database",
+      "nosql",
+      "driver"
+    ],
+    "category": "Database"
   }
 ];
