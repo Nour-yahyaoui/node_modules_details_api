c9d0fe065fff88c3054e3c9884da646163451a04
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 00:39:25 +0200

    Add: Validator - Node.js library docs

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 28
-// Last updated: 2026-08-16 00:39:20
+// Total modules: 29
+// Last updated: 2026-08-16 00:39:25
 
 module.exports = [
   {
@@ -339,5 +339,17 @@ module.exports = [
       "id"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Validator",
+    "description": "String validators and sanitizers for checking email, URL, and other input formats with extensive validation functions",
+    "usage": "const validator = require('validator'); const isValidEmail = validator.isEmail('test@example.com');",
+    "tags": [
+      "validation",
+      "sanitization",
+      "email",
+      "url"
+    ],
+    "category": "Validation"
   }
 ];
