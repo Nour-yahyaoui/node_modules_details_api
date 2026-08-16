8775766a38272eae5f6cf97350d40f4ae2453116
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:14:17 +0200

    Add: Passport to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 22
-// Last updated: 2026-08-16 05:14:12
+// Total modules: 23
+// Last updated: 2026-08-16 05:14:17
 
 module.exports = [
   {
@@ -267,5 +267,17 @@ module.exports = [
       "middleware"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Passport",
+    "description": "Authentication middleware supporting 500+ strategies including OAuth, OpenID, and local authentication",
+    "usage": "const passport = require('passport'); app.use(passport.initialize()); passport.use(new LocalStrategy());",
+    "tags": [
+      "authentication",
+      "oauth",
+      "strategies",
+      "login"
+    ],
+    "category": "Security"
   }
 ];
