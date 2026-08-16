d3599fb3ec7715814f6790aa75a8e5f2064c7dad
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:22:53 +0200

    Add: Shortid to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 102
-// Last updated: 2026-08-16 05:22:47
+// Total modules: 103
+// Last updated: 2026-08-16 05:22:52
 
 module.exports = [
   {
@@ -1225,6 +1225,18 @@ module.exports = [
       "identifier",
       "unique",
       "secure"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Shortid",
+    "description": "Short, non-sequential, URL-friendly unique ID generator with optional customization",
+    "usage": "const shortid = require('shortid'); const id = shortid.generate();",
+    "tags": [
+      "uuid",
+      "identifier",
+      "unique",
+      "short"
     ],
     "category": "Utility"
   }
