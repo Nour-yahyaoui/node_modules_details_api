738b84f6ab6fd8534463e091490fd00e749e4c5c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 01:40:30 +0200

    Add: Hapi Inert to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 234
-// Last updated: 2026-08-17 01:40:25
+// Total modules: 235
+// Last updated: 2026-08-17 01:40:30
 
 module.exports = [
   {
@@ -2811,5 +2811,17 @@ module.exports = [
       "reporter"
     ],
     "category": "Logging"
+  },
+  {
+    "title": "Hapi Inert",
+    "description": "Static file serving plugin for Hapi with directory and file routing support",
+    "usage": "const Hapi = require('@hapi/hapi'); const Inert = require('@hapi/inert'); const server = Hapi.server({ port: 3000 }); await server.register(Inert); server.route({ method: 'GET', path: '/{param*}', handler: { directory: { path: './public' } } });",
+    "tags": [
+      "hapi",
+      "static",
+      "file",
+      "serve"
+    ],
+    "category": "Utility"
   }
 ];
