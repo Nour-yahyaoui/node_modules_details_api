692a6b64e60d7aab007242193a169e7cd7506e9d
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:39:47 +0200

    Add: Zoom API to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 217
-// Last updated: 2026-08-16 05:39:40
+// Total modules: 218
+// Last updated: 2026-08-16 05:39:47
 
 module.exports = [
   {
@@ -2605,6 +2605,18 @@ module.exports = [
       "messaging",
       "api",
       "collaboration"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Zoom API",
+    "description": "Zoom API client for managing meetings, webinars, users, and recordings",
+    "usage": "const ZoomAPI = require('zoomapi'); const zoom = new ZoomAPI({ jwt: 'token' }); const meetings = await zoom.meetings.list({ userId: 'me' });",
+    "tags": [
+      "zoom",
+      "video",
+      "api",
+      "meetings"
     ],
     "category": "Utility"
   }
