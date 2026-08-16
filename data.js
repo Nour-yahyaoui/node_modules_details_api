41b8dbff951e0e729ee492cc448271374c9236eb
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:38:00 +0200

    Add: Undici to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 211
-// Last updated: 2026-08-16 05:37:50
+// Total modules: 212
+// Last updated: 2026-08-16 05:37:59
 
 module.exports = [
   {
@@ -2533,6 +2533,18 @@ module.exports = [
       "fetch",
       "hooks",
       "retry"
+    ],
+    "category": "HTTP Client"
+  },
+  {
+    "title": "Undici",
+    "description": "Fast, efficient HTTP/1.1 and HTTP/2 client with connection pooling and request pipelining",
+    "usage": "const { request } = require('undici'); const { body } = await request('https://api.example.com'); const data = await body.json();",
+    "tags": [
+      "http",
+      "fetch",
+      "fast",
+      "pooling"
     ],
     "category": "HTTP Client"
   }
