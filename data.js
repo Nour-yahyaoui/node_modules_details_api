aa28fee9374ae867af7ca2f963bccfbcc053971a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:27:21 +0200

    Add: Shelljs to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 145
-// Last updated: 2026-08-16 05:27:15
+// Total modules: 146
+// Last updated: 2026-08-16 05:27:20
 
 module.exports = [
   {
@@ -1741,6 +1741,18 @@ module.exports = [
       "exec",
       "promises",
       "cross-platform"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Shelljs",
+    "description": "Cross-platform shell commands with Unix-like syntax for Node.js applications",
+    "usage": "const shell = require('shelljs'); shell.cd('project'); if (!shell.which('git')) shell.echo('Git not found');",
+    "tags": [
+      "shell",
+      "commands",
+      "cross-platform",
+      "unix"
     ],
     "category": "Utility"
   }
