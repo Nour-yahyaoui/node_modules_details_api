6bd26d35fd3ff813fd0ff4c6de2734a19ebb754b
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 01:15:22 +0200

    Add: Handlebars to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 63
-// Last updated: 2026-08-17 01:15:10
+// Total modules: 64
+// Last updated: 2026-08-17 01:15:21
 
 module.exports = [
   {
@@ -759,5 +759,17 @@ module.exports = [
       "persistent"
     ],
     "category": "Dev Tool"
+  },
+  {
+    "title": "Handlebars",
+    "description": "Minimal templating engine with logic-less templates, partials, helpers, and precompilation support",
+    "usage": "const Handlebars = require('handlebars'); const template = Handlebars.compile('Hello {{name}}'); const result = template({ name: 'World' });",
+    "tags": [
+      "templating",
+      "views",
+      "html",
+      "partials"
+    ],
+    "category": "Utility"
   }
 ];
