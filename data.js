1d7a55a943919679cc82ea05cd77cbc5ba661c47
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 01:59:42 +0200

    Add: Prisma Pulse to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 245
-// Last updated: 2026-08-17 01:59:36
+// Total modules: 246
+// Last updated: 2026-08-17 01:59:42
 
 module.exports = [
   {
@@ -2943,5 +2943,17 @@ module.exports = [
       "pooling"
     ],
     "category": "Performance"
+  },
+  {
+    "title": "Prisma Pulse",
+    "description": "Real-time database change stream for Prisma with WebSocket and Redis support",
+    "usage": "import { PrismaClient } from '@prisma/client'; import { withPulse } from '@prisma/pulse'; const prisma = new PrismaClient().$extends(withPulse()); prisma.pulse.subscribe('User', (event) => console.log(event));",
+    "tags": [
+      "prisma",
+      "pulse",
+      "realtime",
+      "change-stream"
+    ],
+    "category": "Real-time"
   }
 ];
