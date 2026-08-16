f265905adca0df28fe98131128a528b03389532c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:32:51 +0200

    Add: Prismic to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 179
-// Last updated: 2026-08-16 05:32:43
+// Total modules: 180
+// Last updated: 2026-08-16 05:32:51
 
 module.exports = [
   {
@@ -2149,6 +2149,18 @@ module.exports = [
       "contentful",
       "content",
       "api"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Prismic",
+    "description": "Prismic headless CMS client with query builder, content management, and slicing",
+    "usage": "const prismic = require('@prismicio/client'); const client = prismic.createClient('repository'); const documents = await client.getAllByType('page');",
+    "tags": [
+      "cms",
+      "prismic",
+      "content",
+      "headless"
     ],
     "category": "Utility"
   }
