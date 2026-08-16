709360fe0b196597ac368398f62c3ae5e64291fd
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 01:37:04 +0200

    Add: Bluebird to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 199
-// Last updated: 2026-08-17 01:36:56
+// Total modules: 200
+// Last updated: 2026-08-17 01:37:04
 
 module.exports = [
   {
@@ -2389,6 +2389,18 @@ module.exports = [
       "control-flow",
       "collections",
       "functional"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Bluebird",
+    "description": "Full-featured Promise library with performance, utilities, and cancellation support",
+    "usage": "const Promise = require('bluebird'); Promise.map([1, 2, 3], n => n * 2).then(results => console.log(results));",
+    "tags": [
+      "promises",
+      "async",
+      "performance",
+      "utilities"
     ],
     "category": "Utility"
   }
