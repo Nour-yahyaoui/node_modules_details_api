0f82f06d3ba1ca48a2892be613817f0d4fbadde7
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-16 05:18:51 +0200

    Add: Apollo Server to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 66
-// Last updated: 2026-08-16 05:18:44
+// Total modules: 67
+// Last updated: 2026-08-16 05:18:51
 
 module.exports = [
   {
@@ -793,6 +793,18 @@ module.exports = [
       "api",
       "query",
       "schema"
+    ],
+    "category": "API"
+  },
+  {
+    "title": "Apollo Server",
+    "description": "GraphQL server for Express, Fastify, and other frameworks with built-in subscriptions and caching",
+    "usage": "const { ApolloServer } = require('@apollo/server'); const server = new ApolloServer({ typeDefs, resolvers });",
+    "tags": [
+      "graphql",
+      "api",
+      "server",
+      "apollo"
     ],
     "category": "API"
   }
