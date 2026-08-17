76d42cf95ff1b4853715c8c4cb4a61fbb8159a16
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 02:00:11 +0200

    Add: Objection Relations to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 250
-// Last updated: 2026-08-17 02:00:05
+// Total modules: 251
+// Last updated: 2026-08-17 02:00:11
 
 module.exports = [
   {
@@ -3001,6 +3001,18 @@ module.exports = [
       "seeding",
       "database",
       "data"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "Objection Relations",
+    "description": "Objection.js relationship management with eager loading and model validation",
+    "usage": "const { Model } = require('objection'); class User extends Model { static get relationMappings() { return { posts: { relation: Model.HasManyRelation, modelClass: Post, join: { from: 'users.id', to: 'posts.user_id' } } }; } } const users = await User.query().withGraphFetched('posts');",
+    "tags": [
+      "objection",
+      "orm",
+      "relations",
+      "eager-loading"
     ],
     "category": "ORM/Database"
   }
