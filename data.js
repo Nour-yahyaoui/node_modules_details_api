b06996ef3174c0d8344c3343635f37636f4cec36
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 02:09:46 +0200

    Add: Express Query to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 325
-// Last updated: 2026-08-17 02:09:40
+// Total modules: 326
+// Last updated: 2026-08-17 02:09:46
 
 module.exports = [
   {
@@ -3903,5 +3903,17 @@ module.exports = [
       "request"
     ],
     "category": "Performance"
+  },
+  {
+    "title": "Express Query",
+    "description": "Query string parsing middleware for Express with custom options",
+    "usage": "const express = require('express'); const app = express(); app.set('query parser', (str) => require('qs').parse(str));",
+    "tags": [
+      "query",
+      "parser",
+      "express",
+      "qs"
+    ],
+    "category": "Utility"
   }
 ];
