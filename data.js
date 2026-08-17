8d962362d731f69543c3db302daaf1ba1c1c8aa5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-17 02:04:12 +0200

    Add: Memoizee to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 286
-// Last updated: 2026-08-17 02:04:06
+// Total modules: 287
+// Last updated: 2026-08-17 02:04:12
 
 module.exports = [
   {
@@ -3433,6 +3433,18 @@ module.exports = [
       "cache",
       "store",
       "cluster"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Memoizee",
+    "description": "Function memoization with configurable cache size and expiration",
+    "usage": "const memoize = require('memoizee'); const fn = memoize((a, b) => a + b, { maxAge: 60000, max: 100 });",
+    "tags": [
+      "memoization",
+      "cache",
+      "performance",
+      "function"
     ],
     "category": "Performance"
   }
