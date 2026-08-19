6c1851485bb1ccf7242b3e372fd424381e248794
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-19 05:56:34 +0200

    Add: SWC to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 160
-// Last updated: 2026-08-19 05:56:28
+// Total modules: 161
+// Last updated: 2026-08-19 05:56:34
 
 module.exports = [
   {
@@ -1921,6 +1921,18 @@ module.exports = [
       "typing",
       "compiler",
       "language"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "SWC",
+    "description": "Extremely fast Rust-based JavaScript/TypeScript compiler for building and bundling",
+    "usage": "npx swc src -d dist",
+    "tags": [
+      "compiler",
+      "fast",
+      "rust",
+      "typescript"
     ],
     "category": "Dev Tool"
   }
