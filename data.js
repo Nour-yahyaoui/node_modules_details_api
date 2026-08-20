7f85b1b4e843679b6e9d2748c8967ff6d768e6cc
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 03:16:44 +0200

    Add: QS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 179
-// Last updated: 2026-08-20 03:16:38
+// Total modules: 180
+// Last updated: 2026-08-20 03:16:44
 
 module.exports = [
   {
@@ -2149,6 +2149,18 @@ module.exports = [
       "parser",
       "express",
       "qs"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "QS",
+    "description": "Query string parsing and stringifying with nested objects support",
+    "usage": "const qs = require('qs'); const obj = qs.parse('a=b&c=d'); const str = qs.stringify(obj);",
+    "tags": [
+      "qs",
+      "query",
+      "parse",
+      "stringify"
     ],
     "category": "Utility"
   }
