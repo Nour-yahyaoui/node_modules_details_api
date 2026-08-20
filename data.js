f52146534816d175b6cdfaf4aeabe0794d6036c9
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 04:09:46 +0200

    Add: Humanize Duration to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 202
-// Last updated: 2026-08-20 04:09:40
+// Total modules: 203
+// Last updated: 2026-08-20 04:09:45
 
 module.exports = [
   {
@@ -2425,6 +2425,18 @@ module.exports = [
       "async",
       "performance",
       "utilities"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Humanize Duration",
+    "description": "Convert milliseconds to human-readable duration strings with internationalization",
+    "usage": "const humanizeDuration = require('humanize-duration'); console.log(humanizeDuration(3600000)); // '1 hour'",
+    "tags": [
+      "time",
+      "duration",
+      "format",
+      "human-readable"
     ],
     "category": "Utility"
   }
