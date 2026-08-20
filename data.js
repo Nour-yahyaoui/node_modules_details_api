1044f345b87675b0ce39f353687e510efb2eb749
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 04:04:58 +0200

    Add: Node-emoji to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 152
-// Last updated: 2026-08-20 04:04:52
+// Total modules: 153
+// Last updated: 2026-08-20 04:04:57
 
 module.exports = [
   {
@@ -1825,6 +1825,18 @@ module.exports = [
       "pty",
       "shell",
       "emulation"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Node-emoji",
+    "description": "Emoji support with conversion between emoji names, characters, and Unicode representations",
+    "usage": "const emoji = require('node-emoji'); console.log(emoji.emojify('I :heart: Node.js')); console.log(emoji.get('heart'));",
+    "tags": [
+      "emoji",
+      "unicode",
+      "conversion",
+      "unicode"
     ],
     "category": "Utility"
   }
