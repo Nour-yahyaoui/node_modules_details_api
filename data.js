36ba94ba13e7d0a68c8f2bee62687780a675438e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 03:59:27 +0200

    Add: Memory-cache to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 99
-// Last updated: 2026-08-20 03:59:21
+// Total modules: 100
+// Last updated: 2026-08-20 03:59:27
 
 module.exports = [
   {
@@ -1189,6 +1189,18 @@ module.exports = [
       "memory",
       "ttl",
       "performance"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Memory-cache",
+    "description": "Lightweight in-memory cache with simple put/get API and expiration support",
+    "usage": "const cache = require('memory-cache'); cache.put('key', 'value', 60000); const value = cache.get('key');",
+    "tags": [
+      "cache",
+      "memory",
+      "ttl",
+      "simple"
     ],
     "category": "Performance"
   }
