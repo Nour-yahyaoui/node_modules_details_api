bf3afb6bf5170726f89fb7d0ac633650f7b38be5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 04:02:58 +0200

    Add: Nock to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 133
-// Last updated: 2026-08-20 04:02:52
+// Total modules: 134
+// Last updated: 2026-08-20 04:02:58
 
 module.exports = [
   {
@@ -1597,6 +1597,18 @@ module.exports = [
       "http",
       "assertions",
       "express"
+    ],
+    "category": "Testing/Tool"
+  },
+  {
+    "title": "Nock",
+    "description": "HTTP mocking library for testing with interception, recording, and custom responses",
+    "usage": "const nock = require('nock'); nock('https://api.example.com').get('/users').reply(200, { users: [] });",
+    "tags": [
+      "testing",
+      "mock",
+      "http",
+      "interception"
     ],
     "category": "Testing/Tool"
   }
