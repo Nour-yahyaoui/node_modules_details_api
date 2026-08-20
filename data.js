52a67855437527e2ae60f4378c129ca2589cfe44
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-20 04:17:31 +0200

    Add: Objection Query Builder to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 273
-// Last updated: 2026-08-20 04:17:17
+// Total modules: 274
+// Last updated: 2026-08-20 04:17:30
 
 module.exports = [
   {
@@ -3277,6 +3277,18 @@ module.exports = [
       "query",
       "builder",
       "sql"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "Objection Query Builder",
+    "description": "Objection.js fluent query builder with Eager loading and raw SQL support",
+    "usage": "const users = await User.query() .where('age', '>', 18) .select('name', 'email') .orderBy('name');",
+    "tags": [
+      "objection",
+      "query",
+      "builder",
+      "eager-loading"
     ],
     "category": "ORM/Database"
   }
