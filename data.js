dca0983b76e976cb89e5ec10f695234b63d3eed0
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 04:30:33 +0200

    Add: Azure SDK to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 68
-// Last updated: 2026-08-21 04:30:27
+// Total modules: 69
+// Last updated: 2026-08-21 04:30:33
 
 module.exports = [
   {
@@ -817,6 +817,18 @@ module.exports = [
       "gcp",
       "storage",
       "firestore"
+    ],
+    "category": "Cloud"
+  },
+  {
+    "title": "Azure SDK",
+    "description": "Microsoft Azure SDK with support for Blob Storage, Queue, Service Bus, and Cosmos DB services",
+    "usage": "const { BlobServiceClient } = require('@azure/storage-blob'); const client = BlobServiceClient.fromConnectionString(connectionString);",
+    "tags": [
+      "azure",
+      "microsoft",
+      "cloud",
+      "storage"
     ],
     "category": "Cloud"
   }
