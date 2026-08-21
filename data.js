5c3d15f10511fdd89d7253c194a3c84b66a6f2ec
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 04:31:37 +0200

    Add: Lowdb to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 79
-// Last updated: 2026-08-21 04:31:32
+// Total modules: 80
+// Last updated: 2026-08-21 04:31:37
 
 module.exports = [
   {
@@ -949,6 +949,18 @@ module.exports = [
       "key-value",
       "leveldb",
       "embedded"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "Lowdb",
+    "description": "Small local database for Node.js using JSON files with lodash query support and plugins",
+    "usage": "const low = require('lowdb'); const FileSync = require('lowdb/adapters/FileSync'); const adapter = new FileSync('db.json'); const db = low(adapter); db.defaults({ posts: [] }).write();",
+    "tags": [
+      "database",
+      "json",
+      "local",
+      "file"
     ],
     "category": "Database"
   }
