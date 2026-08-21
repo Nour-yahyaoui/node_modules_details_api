139cdb3abe0163b9929283142f7eb9ca247dd090
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 03:35:50 +0200

    Add: ESLint to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 10
-// Last updated: 2026-08-21 03:35:42
+// Total modules: 11
+// Last updated: 2026-08-21 03:35:50
 
 module.exports = [
   {
@@ -122,5 +122,17 @@ module.exports = [
       "variables"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "ESLint",
+    "description": "Static code analysis tool for identifying and fixing problems in JavaScript and TypeScript code",
+    "usage": "npx eslint --fix .",
+    "tags": [
+      "lint",
+      "code-quality",
+      "static-analysis",
+      "typescript"
+    ],
+    "category": "Dev Tool"
   }
 ];
