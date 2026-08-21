05de1b234b0ee037727642317807d8da025bff3f
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 04:49:45 +0200

    Add: Hapi Boom to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 183
-// Last updated: 2026-08-21 04:49:40
+// Total modules: 184
+// Last updated: 2026-08-21 04:49:45
 
 module.exports = [
   {
@@ -2199,5 +2199,17 @@ module.exports = [
       "security"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Hapi Boom",
+    "description": "HTTP-friendly error objects for Hapi with status codes and error messages",
+    "usage": "const Hapi = require('@hapi/hapi'); const Boom = require('@hapi/boom'); const server = Hapi.server({ port: 3000 }); server.route({ method: 'GET', path: '/', handler: () => { throw Boom.notFound('Not found'); } });",
+    "tags": [
+      "hapi",
+      "errors",
+      "http",
+      "boom"
+    ],
+    "category": "Utility"
   }
 ];
