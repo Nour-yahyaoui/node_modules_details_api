858ba7d21de5c4235cde8e9b6587dd179ff9ae64
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 05:02:47 +0200

    Add: Transform Stream to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 249
-// Last updated: 2026-08-21 05:02:41
+// Total modules: 250
+// Last updated: 2026-08-21 05:02:46
 
 module.exports = [
   {
@@ -2988,6 +2988,18 @@ module.exports = [
       "stream",
       "writable",
       "write",
+      "data"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Transform Stream",
+    "description": "Transform stream for data modification between read and write",
+    "usage": "const { Transform } = require('stream'); const upper = new Transform({ transform(chunk, encoding, callback) { this.push(chunk.toString().toUpperCase()); callback(); } });",
+    "tags": [
+      "stream",
+      "transform",
+      "modify",
       "data"
     ],
     "category": "Utility"
