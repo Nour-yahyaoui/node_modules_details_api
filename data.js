4839045ed788d8cf3b8f20b42dcdc0a9715dbd80
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 03:43:51 +0200

    Add: Chai to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 41
-// Last updated: 2026-08-21 03:43:45
+// Total modules: 42
+// Last updated: 2026-08-21 03:43:50
 
 module.exports = [
   {
@@ -493,6 +493,18 @@ module.exports = [
       "unit-test",
       "async",
       "reporting"
+    ],
+    "category": "Testing/Tool"
+  },
+  {
+    "title": "Chai",
+    "description": "BDD assertion library with expect, should, and assert interfaces for Node.js and browsers",
+    "usage": "const expect = require('chai').expect; expect(2 + 2).to.equal(4);",
+    "tags": [
+      "testing",
+      "assertions",
+      "bdd",
+      "chai"
     ],
     "category": "Testing/Tool"
   }
