c61db10d0340bed26a251ef010eac39651cae6ef
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 03:39:07 +0200

    Add: UUID to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 25
-// Last updated: 2026-08-21 03:38:57
+// Total modules: 26
+// Last updated: 2026-08-21 03:39:06
 
 module.exports = [
   {
@@ -303,5 +303,17 @@ module.exports = [
       "broadcast"
     ],
     "category": "Real-time"
+  },
+  {
+    "title": "UUID",
+    "description": "Simple and fast RFC-compliant UUID generation with support for v1, v3, v4, and v5 UUIDs",
+    "usage": "const { v4: uuidv4 } = require('uuid'); const id = uuidv4();",
+    "tags": [
+      "uuid",
+      "identifier",
+      "unique",
+      "id"
+    ],
+    "category": "Utility"
   }
 ];
