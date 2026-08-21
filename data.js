acec8fe71f0b6555b05ab2293f173a427fb62c9c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 04:49:10 +0200

    Add: Humanize Duration to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
 // Total modules: 180
-// Last updated: 2026-08-21 04:49:09
+// Last updated: 2026-08-21 04:49:10
 
 module.exports = [
   {
@@ -2153,14 +2153,14 @@ module.exports = [
     "category": "Utility"
   },
   {
-    "title": "Bluebird",
-    "description": "Full-featured Promise library with performance, utilities, and cancellation support",
-    "usage": "const Promise = require('bluebird'); Promise.map([1, 2, 3], n => n * 2).then(results => console.log(results));",
+    "title": "Humanize Duration",
+    "description": "Convert milliseconds to human-readable duration strings with internationalization",
+    "usage": "const humanizeDuration = require('humanize-duration'); console.log(humanizeDuration(3600000)); // '1 hour'",
     "tags": [
-      "promises",
-      "async",
-      "performance",
-      "utilities"
+      "time",
+      "duration",
+      "format",
+      "human-readable"
     ],
     "category": "Utility"
   }
