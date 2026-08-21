a5831eeaa3b1cc8c1b8b9f5c4de750e87a50bcfe
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-21 04:49:21 +0200

    Add: Fastify Static to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
 // Total modules: 180
-// Last updated: 2026-08-21 04:49:20
+// Last updated: 2026-08-21 04:49:21
 
 module.exports = [
   {
@@ -2153,14 +2153,14 @@ module.exports = [
     "category": "Utility"
   },
   {
-    "title": "Fastify Sensible",
-    "description": "Collection of useful Fastify plugins including defaults, assertions, and utilities",
-    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/sensible'));",
+    "title": "Fastify Static",
+    "description": "Static file serving plugin for Fastify with caching and path options",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/static'), { root: './public' });",
     "tags": [
       "fastify",
-      "utilities",
-      "assert",
-      "middleware"
+      "static",
+      "file",
+      "serve"
     ],
     "category": "Utility"
   }
