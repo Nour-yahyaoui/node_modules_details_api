01cf76ec052333d49f35a65ea141a8d48c044790
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:43:44 +0200

    Add: HPP to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 312
-// Last updated: 2026-08-22 05:43:38
+// Total modules: 313
+// Last updated: 2026-08-22 05:43:44
 
 module.exports = [
   {
@@ -3745,6 +3745,18 @@ module.exports = [
       "html",
       "sanitization",
       "output"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "HPP",
+    "description": "HTTP Parameter Pollution protection middleware for Express",
+    "usage": "const hpp = require('hpp'); app.use(hpp());",
+    "tags": [
+      "hpp",
+      "security",
+      "parameters",
+      "express"
     ],
     "category": "Security"
   }
