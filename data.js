d1d1c88e7728b6cbfd09447bf6307864ec12770d
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:39:05 +0200

    Add: Sequelize Seeders to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 268
-// Last updated: 2026-08-22 05:38:59
+// Total modules: 269
+// Last updated: 2026-08-22 05:39:05
 
 module.exports = [
   {
@@ -3217,6 +3217,18 @@ module.exports = [
       "migrations",
       "database",
       "schema"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "Sequelize Seeders",
+    "description": "Sequelize data seeding with bulk insert and transaction support",
+    "usage": "module.exports = { up: async (queryInterface) => { await queryInterface.bulkInsert('Users', [{ name: 'John' }]); }, down: async (queryInterface) => { await queryInterface.bulkDelete('Users', null, {}); } };",
+    "tags": [
+      "sequelize",
+      "seeders",
+      "data",
+      "database"
     ],
     "category": "ORM/Database"
   }
