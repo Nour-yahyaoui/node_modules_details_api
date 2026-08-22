e9aa9a85aca56a8caf19deedb3d657849a9b1a53
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:25:01 +0200

    Add: Bunyan to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 139
-// Last updated: 2026-08-22 05:24:54
+// Total modules: 140
+// Last updated: 2026-08-22 05:25:01
 
 module.exports = [
   {
@@ -1669,6 +1669,18 @@ module.exports = [
       "pino",
       "pretty",
       "console"
+    ],
+    "category": "Logging"
+  },
+  {
+    "title": "Bunyan",
+    "description": "JSON logging library with CLI, ring buffer, and rotation support for Node.js applications",
+    "usage": "const bunyan = require('bunyan'); const logger = bunyan.createLogger({ name: 'myapp' }); logger.info({ user: 'john' }, 'User logged in');",
+    "tags": [
+      "logging",
+      "json",
+      "bunyan",
+      "debug"
     ],
     "category": "Logging"
   }
