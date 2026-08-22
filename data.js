23f6709c850b05b82ef0f345e18af5864bf76f13
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:39:34 +0200

    Add: TypeORM Query Builder to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 273
-// Last updated: 2026-08-22 05:39:28
+// Total modules: 274
+// Last updated: 2026-08-22 05:39:34
 
 module.exports = [
   {
@@ -3277,6 +3277,18 @@ module.exports = [
       "unit-of-work",
       "identity-map",
       "transaction"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "TypeORM Query Builder",
+    "description": "TypeORM query builder with complex conditions, joins, and subqueries",
+    "usage": "const users = await getRepository(User).createQueryBuilder('user') .leftJoinAndSelect('user.posts', 'post') .where('user.age > :age', { age: 18 }) .getMany();",
+    "tags": [
+      "typeorm",
+      "query",
+      "builder",
+      "sql"
     ],
     "category": "ORM/Database"
   }
