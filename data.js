859b407acefbbd8c104739740ccbe06e3bb64480
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 16:24:22 +0200

    Add: Objection.js to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 87
-// Last updated: 2026-08-22 16:24:17
+// Total modules: 88
+// Last updated: 2026-08-22 16:24:22
 
 module.exports = [
   {
@@ -1045,6 +1045,18 @@ module.exports = [
       "query",
       "typescript",
       "builder"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "Objection.js",
+    "description": "SQL ORM with intuitive model definitions, relation management, and query building for Node.js",
+    "usage": "const { Model } = require('objection'); class User extends Model { static get tableName() { return 'users'; } } const users = await User.query().where('age', '>', 18);",
+    "tags": [
+      "orm",
+      "sql",
+      "models",
+      "relations"
     ],
     "category": "ORM/Database"
   }
