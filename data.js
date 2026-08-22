39147f2cff7e244721cdc84562f74d44624a283f
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:34:04 +0200

    Add: Socks to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 216
-// Last updated: 2026-08-22 05:33:58
+// Total modules: 217
+// Last updated: 2026-08-22 05:34:04
 
 module.exports = [
   {
@@ -2595,5 +2595,17 @@ module.exports = [
       "compression"
     ],
     "category": "HTTP Client"
+  },
+  {
+    "title": "Socks",
+    "description": "SOCKS proxy client for creating connections through proxy servers with authentication",
+    "usage": "const socks = require('socks'); const info = await socks.createConnection({ proxy: { ipaddress: '127.0.0.1', port: 1080 }, target: { host: 'example.com', port: 80 } });",
+    "tags": [
+      "proxy",
+      "socks",
+      "networking",
+      "connection"
+    ],
+    "category": "Utility"
   }
 ];
