9de3f0128e183d9cb7b9502e872aad4ccfcb6326
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:36:18 +0200

    Add: NestJS Mongoose to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 239
-// Last updated: 2026-08-22 05:36:12
+// Total modules: 240
+// Last updated: 2026-08-22 05:36:18
 
 module.exports = [
   {
@@ -2871,5 +2871,17 @@ module.exports = [
       "handlebars"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "NestJS Mongoose",
+    "description": "Mongoose integration for NestJS with schema definitions and model injection",
+    "usage": "import { Module } from '@nestjs/common'; import { MongooseModule } from '@nestjs/mongoose'; @Module({ imports: [MongooseModule.forRoot('mongodb://localhost:27017/test')] }) export class AppModule {}",
+    "tags": [
+      "nestjs",
+      "mongodb",
+      "mongoose",
+      "orm"
+    ],
+    "category": "ORM/Database"
   }
 ];
