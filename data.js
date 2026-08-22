1aafd1b9eee0514c709da74011bd4c2dd8ac160e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:23:55 +0200

    Add: JSON GraphQL Server to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 128
-// Last updated: 2026-08-22 05:23:49
+// Total modules: 129
+// Last updated: 2026-08-22 05:23:55
 
 module.exports = [
   {
@@ -1537,6 +1537,18 @@ module.exports = [
       "websocket",
       "subscriptions",
       "realtime"
+    ],
+    "category": "API"
+  },
+  {
+    "title": "JSON GraphQL Server",
+    "description": "Zero-config GraphQL server that automatically generates CRUD operations from JSON data",
+    "usage": "const jsonGraphqlServer = require('json-graphql-server'); const server = jsonGraphqlServer({ posts: [{ id: 1, title: 'Hello' }] });",
+    "tags": [
+      "graphql",
+      "json",
+      "crud",
+      "mock"
     ],
     "category": "API"
   }
