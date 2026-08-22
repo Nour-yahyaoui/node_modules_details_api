19e271b2fce47a7e4364b2c814328f04b1214e5c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:15:58 +0200

    Add: Webpack to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 46
-// Last updated: 2026-08-22 05:15:53
+// Total modules: 47
+// Last updated: 2026-08-22 05:15:58
 
 module.exports = [
   {
@@ -553,6 +553,18 @@ module.exports = [
       "build",
       "dev-server",
       "hmr"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Webpack",
+    "description": "Powerful module bundler that transforms JavaScript, assets, and CSS into optimized production bundles",
+    "usage": "webpack --config webpack.config.js",
+    "tags": [
+      "bundler",
+      "build",
+      "assets",
+      "loader"
     ],
     "category": "Dev Tool"
   }
