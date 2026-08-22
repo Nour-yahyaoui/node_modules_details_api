07997023de7906b475dfe76cc8fafe8f1a74a9c6
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 16:20:48 +0200

    Add: GraphQL to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 65
-// Last updated: 2026-08-22 16:20:41
+// Total modules: 66
+// Last updated: 2026-08-22 16:20:48
 
 module.exports = [
   {
@@ -783,5 +783,17 @@ module.exports = [
       "syntax"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "GraphQL",
+    "description": "GraphQL reference implementation with schema construction, query parsing, validation, and execution",
+    "usage": "const { graphql, buildSchema } = require('graphql'); const schema = buildSchema('type Query { hello: String }'); const result = await graphql(schema, '{ hello }', { hello: () => 'World' });",
+    "tags": [
+      "graphql",
+      "api",
+      "query",
+      "schema"
+    ],
+    "category": "API"
   }
 ];
