7f955fad52879037e1dbbdcae0df0abb7577f6ea
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 17:11:21 +0200

    Add: Mercurius to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 63
-// Last updated: 2026-08-22 17:11:14
+// Total modules: 64
+// Last updated: 2026-08-22 17:11:20
 
 module.exports = [
   {
@@ -757,6 +757,18 @@ module.exports = [
       "express",
       "middleware",
       "graphiql"
+    ],
+    "category": "API"
+  },
+  {
+    "title": "Mercurius",
+    "description": "GraphQL server and gateway for Fastify with schema stitching, federation, and caching capabilities",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('mercurius'), { schema });",
+    "tags": [
+      "graphql",
+      "fastify",
+      "federation",
+      "gateway"
     ],
     "category": "API"
   }
