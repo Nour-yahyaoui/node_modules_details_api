2db4b4d605c5eea896ce92d08f2a8a1700151227
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 16:25:13 +0200

    Add: Superagent to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 95
-// Last updated: 2026-08-22 16:25:06
+// Total modules: 96
+// Last updated: 2026-08-22 16:25:13
 
 module.exports = [
   {
@@ -1141,6 +1141,18 @@ module.exports = [
       "fetch",
       "promises",
       "api"
+    ],
+    "category": "HTTP Client"
+  },
+  {
+    "title": "Superagent",
+    "description": "Progressive HTTP client with flexible API, plugins, and support for query strings and multipart requests",
+    "usage": "const superagent = require('superagent'); const response = await superagent.get('https://api.example.com/data').query({ limit: 10 });",
+    "tags": [
+      "http",
+      "api",
+      "request",
+      "promises"
     ],
     "category": "HTTP Client"
   }
