1aabc51fe7d1f0335d6be1ea49dd3bfe222b1d4f
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-22 05:24:54 +0200

    Add: Pino-pretty to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 138
-// Last updated: 2026-08-22 05:24:48
+// Total modules: 139
+// Last updated: 2026-08-22 05:24:54
 
 module.exports = [
   {
@@ -1659,5 +1659,17 @@ module.exports = [
       "data"
     ],
     "category": "Testing/Tool"
+  },
+  {
+    "title": "Pino-pretty",
+    "description": "Pretty logger for Pino with colorized output, human-readable timestamps, and log formatting",
+    "usage": "const pino = require('pino'); const logger = pino({ transport: { target: 'pino-pretty' } }); logger.info('Hello World');",
+    "tags": [
+      "logging",
+      "pino",
+      "pretty",
+      "console"
+    ],
+    "category": "Logging"
   }
 ];
