59fdb0e3b7b09243159f503132ef5357d231ddb5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:30:10 +0200

    Add: Terraform (CDKTF) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 166
-// Last updated: 2026-08-23 05:30:04
+// Total modules: 167
+// Last updated: 2026-08-23 05:30:10
 
 module.exports = [
   {
@@ -1993,6 +1993,18 @@ module.exports = [
       "k8s",
       "orchestration",
       "containers"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Terraform (CDKTF)",
+    "description": "Cloud Development Kit for Terraform with infrastructure as code using TypeScript and JavaScript",
+    "usage": "import { Construct } from 'constructs'; import { App, TerraformStack } from 'cdktf';",
+    "tags": [
+      "terraform",
+      "iac",
+      "cloud",
+      "infrastructure"
     ],
     "category": "Dev Tool"
   }
