660d1399ff345985dc9c3a53ea5451d8b95c7959
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:54:04 +0200

    Add: Fastify Argon2 to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 411
-// Last updated: 2026-08-23 05:53:58
+// Total modules: 412
+// Last updated: 2026-08-23 05:54:04
 
 module.exports = [
   {
@@ -4931,6 +4931,18 @@ module.exports = [
     "tags": [
       "fastify",
       "bcrypt",
+      "hash",
+      "security"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "Fastify Argon2",
+    "description": "Argon2 hashing plugin for Fastify with memory and time parameters",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-argon2'), { memoryCost: 1024, timeCost: 2 });",
+    "tags": [
+      "fastify",
+      "argon2",
       "hash",
       "security"
     ],
