0d67136179b8b0cf975792b566cdd960d6eb2a80
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:57:14 +0200

    Add: Fastify Analytics to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 439
-// Last updated: 2026-08-23 05:57:08
+// Total modules: 440
+// Last updated: 2026-08-23 05:57:14
 
 module.exports = [
   {
@@ -5269,6 +5269,18 @@ module.exports = [
       "ab-testing",
       "experiments",
       "allocation"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Analytics",
+    "description": "Analytics plugin for Fastify with event tracking and aggregation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-analytics'), { providers: ['google', 'mixpanel'] });",
+    "tags": [
+      "fastify",
+      "analytics",
+      "tracking",
+      "metrics"
     ],
     "category": "Utility"
   }
