af64ab795c68f139c8791158053f568df30934d1
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:37:45 +0200

    Add: NestJS TypeORM to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 241
-// Last updated: 2026-08-23 05:37:39
+// Total modules: 242
+// Last updated: 2026-08-23 05:37:45
 
 module.exports = [
   {
@@ -2892,6 +2892,18 @@ module.exports = [
       "nestjs",
       "mongodb",
       "mongoose",
+      "orm"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "NestJS TypeORM",
+    "description": "TypeORM integration for NestJS with entities, repositories, and migrations",
+    "usage": "import { Module } from '@nestjs/common'; import { TypeOrmModule } from '@nestjs/typeorm'; @Module({ imports: [TypeOrmModule.forRoot({ type: 'postgres', host: 'localhost', database: 'test' })] }) export class AppModule {}",
+    "tags": [
+      "nestjs",
+      "typeorm",
+      "database",
       "orm"
     ],
     "category": "ORM/Database"
