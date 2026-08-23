d8e2bb00598f0857a6c999bbb60a7386098959be
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:49:58 +0200

    Add: Fastify Cache to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 369
-// Last updated: 2026-08-23 05:49:52
+// Total modules: 370
+// Last updated: 2026-08-23 05:49:57
 
 module.exports = [
   {
@@ -4429,6 +4429,18 @@ module.exports = [
       "circuit-breaker",
       "resilience",
       "failure"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Fastify Cache",
+    "description": "Cache plugin for Fastify with in-memory and Redis storage",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-cache'), { storage: 'memory', ttl: 60 });",
+    "tags": [
+      "fastify",
+      "cache",
+      "performance",
+      "ttl"
     ],
     "category": "Performance"
   }
