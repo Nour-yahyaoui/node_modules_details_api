05c24f69b6d11043d3d554be0c1d2079baca64fa
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 06:03:48 +0200

    Add: Fastify API Gateway to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 491
-// Last updated: 2026-08-23 06:03:42
+// Total modules: 492
+// Last updated: 2026-08-23 06:03:48
 
 module.exports = [
   {
@@ -5893,6 +5893,18 @@ module.exports = [
       "appsync",
       "graphql",
       "aws"
+    ],
+    "category": "Cloud"
+  },
+  {
+    "title": "Fastify API Gateway",
+    "description": "AWS API Gateway plugin for Fastify with REST and WebSocket support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-api-gateway'), { region: 'us-east-1' });",
+    "tags": [
+      "fastify",
+      "api-gateway",
+      "aws",
+      "rest"
     ],
     "category": "Cloud"
   }
