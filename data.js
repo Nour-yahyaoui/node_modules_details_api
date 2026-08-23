26425a7245f8c7a3681260b276db69ee2d95dc1e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:52:34 +0200

    Add: Fastify Winston to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 396
-// Last updated: 2026-08-23 05:52:28
+// Total modules: 397
+// Last updated: 2026-08-23 05:52:34
 
 module.exports = [
   {
@@ -4753,6 +4753,18 @@ module.exports = [
       "logging",
       "pino",
       "pretty"
+    ],
+    "category": "Logging"
+  },
+  {
+    "title": "Fastify Winston",
+    "description": "Winston logging plugin for Fastify with multiple transports",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-winston'), { transports: [new winston.transports.Console()] });",
+    "tags": [
+      "fastify",
+      "winston",
+      "logging",
+      "transports"
     ],
     "category": "Logging"
   }
