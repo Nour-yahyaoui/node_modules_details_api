1475f8eb1951ec19df6145912dbdbf69a8cd6fe4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:50:09 +0200

    Add: Fastify Secure Session to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 371
-// Last updated: 2026-08-23 05:50:03
+// Total modules: 372
+// Last updated: 2026-08-23 05:50:09
 
 module.exports = [
   {
@@ -4455,5 +4455,17 @@ module.exports = [
       "snowflake"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Fastify Secure Session",
+    "description": "Secure session plugin for Fastify with encryption and signed cookies",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/secure-session'), { key: Buffer.from('secret') });",
+    "tags": [
+      "fastify",
+      "session",
+      "security",
+      "encryption"
+    ],
+    "category": "Security"
   }
 ];
