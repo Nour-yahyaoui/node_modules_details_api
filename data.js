88dd849a1152ba6b11064981c3f7f993624b1112
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 06:04:32 +0200

    Add: Fastify ECS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 496
-// Last updated: 2026-08-23 06:04:15
+// Total modules: 497
+// Last updated: 2026-08-23 06:04:32
 
 module.exports = [
   {
@@ -5953,6 +5953,18 @@ module.exports = [
       "parameter-store",
       "aws",
       "configuration"
+    ],
+    "category": "Cloud"
+  },
+  {
+    "title": "Fastify ECS",
+    "description": "AWS ECS plugin for Fastify with container management",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-ecs'), { region: 'us-east-1' });",
+    "tags": [
+      "fastify",
+      "ecs",
+      "aws",
+      "containers"
     ],
     "category": "Cloud"
   }
