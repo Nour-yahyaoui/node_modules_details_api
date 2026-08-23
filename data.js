ced23f10312d52b5492163d07b01a2b543603d80
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:55:31 +0200

    Add: Fastify Excel to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 425
-// Last updated: 2026-08-23 05:55:26
+// Total modules: 426
+// Last updated: 2026-08-23 05:55:31
 
 module.exports = [
   {
@@ -5103,5 +5103,17 @@ module.exports = [
       "schema"
     ],
     "category": "Validation"
+  },
+  {
+    "title": "Fastify Excel",
+    "description": "Excel generation plugin for Fastify with xlsx and csv support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-excel'), { });",
+    "tags": [
+      "fastify",
+      "excel",
+      "xlsx",
+      "csv"
+    ],
+    "category": "Utility"
   }
 ];
