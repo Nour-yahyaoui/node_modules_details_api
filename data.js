032cf53a966fd3414d690f22e2784293683ddfb2
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:54:51 +0200

    Add: Fastify Censor to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 418
-// Last updated: 2026-08-23 05:54:45
+// Total modules: 419
+// Last updated: 2026-08-23 05:54:51
 
 module.exports = [
   {
@@ -5019,5 +5019,17 @@ module.exports = [
       "xss"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Fastify Censor",
+    "description": "Profanity filter plugin for Fastify with custom word lists",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-censor'), { words: ['badword'] });",
+    "tags": [
+      "fastify",
+      "censor",
+      "profanity",
+      "filter"
+    ],
+    "category": "Utility"
   }
 ];
