0d20ab38e0661d97dff0625e59145c9e36366c27
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:47:14 +0200

    Add: YAML to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 340
-// Last updated: 2026-08-23 05:47:08
+// Total modules: 341
+// Last updated: 2026-08-23 05:47:14
 
 module.exports = [
   {
@@ -4081,6 +4081,18 @@ module.exports = [
       "express",
       "callback",
       "cross-domain"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "YAML",
+    "description": "YAML parser and stringifier for Node.js with custom schemas",
+    "usage": "const yaml = require('yaml'); const obj = yaml.parse('key: value'); const str = yaml.stringify(obj);",
+    "tags": [
+      "yaml",
+      "parse",
+      "stringify",
+      "configuration"
     ],
     "category": "Utility"
   }
