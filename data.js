56fdaf427b179a08d4c1af7d839eaa9c000f7613
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:43:20 +0200

    Add: Stream Pipes to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 300
-// Last updated: 2026-08-23 05:43:14
+// Total modules: 301
+// Last updated: 2026-08-23 05:43:19
 
 module.exports = [
   {
@@ -3601,6 +3601,18 @@ module.exports = [
       "gzip",
       "deflate",
       "compression"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Stream Pipes",
+    "description": "Node.js stream pipeline with promise support and error handling",
+    "usage": "const { pipeline } = require('stream/promises'); await pipeline(fs.createReadStream('input.txt'), zlib.createGzip(), fs.createWriteStream('output.gz'));",
+    "tags": [
+      "stream",
+      "pipeline",
+      "async",
+      "promises"
     ],
     "category": "Utility"
   }
