aa3e85ebe604b13d2859f0100e590a1e819ae584
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:39:59 +0200

    Add: Sequelize Validations to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 265
-// Last updated: 2026-08-23 05:39:53
+// Total modules: 266
+// Last updated: 2026-08-23 05:39:59
 
 module.exports = [
   {
@@ -3181,6 +3181,18 @@ module.exports = [
       "associations",
       "orm",
       "models"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "Sequelize Validations",
+    "description": "Sequelize model validations with built-in and custom validators",
+    "usage": "const User = sequelize.define('User', { name: { type: DataTypes.STRING, validate: { len: [3, 20] } }, email: { type: DataTypes.STRING, validate: { isEmail: true } } });",
+    "tags": [
+      "sequelize",
+      "validations",
+      "models",
+      "database"
     ],
     "category": "ORM/Database"
   }
