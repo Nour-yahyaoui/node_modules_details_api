2821e00affab71a5c893e5466d2222296343958f
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-23 05:56:01 +0200

    Add: Fastify YAML (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 430
-// Last updated: 2026-08-23 05:55:55
+// Total modules: 431
+// Last updated: 2026-08-23 05:56:01
 
 module.exports = [
   {
@@ -5161,6 +5161,18 @@ module.exports = [
       "xml",
       "parse",
       "generate"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify YAML (Advanced)",
+    "description": "Advanced YAML plugin with custom schemas and validation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-yaml'), { schema: 'core' });",
+    "tags": [
+      "fastify",
+      "yaml",
+      "parse",
+      "schema"
     ],
     "category": "Utility"
   }
