391c1ca4ff75b53e36df3ed074f52029b2bb311c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:27:36 +0200

    Add: Jsonwebtoken to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 18
-// Last updated: 2026-08-25 08:27:34
+// Total modules: 19
+// Last updated: 2026-08-25 08:27:36
 
 module.exports = [
   {
@@ -219,5 +219,17 @@ module.exports = [
       "types"
     ],
     "category": "Validation"
+  },
+  {
+    "title": "Jsonwebtoken",
+    "description": "Implementation of JSON Web Tokens for creating and verifying authentication tokens with HMAC, RSA, and ECDSA",
+    "usage": "const jwt = require('jsonwebtoken'); const token = jwt.sign({ userId: 123 }, 'secret');",
+    "tags": [
+      "jwt",
+      "authentication",
+      "security",
+      "tokens"
+    ],
+    "category": "Security"
   }
 ];
