5d30a21b42d171848f0a94b30e8ce9dc9e0a959a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:33:04 +0200

    Add: Zlib to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 299
-// Last updated: 2026-08-25 08:33:02
+// Total modules: 300
+// Last updated: 2026-08-25 08:33:03
 
 module.exports = [
   {
@@ -3591,5 +3591,17 @@ module.exports = [
       "performance"
     ],
     "category": "Performance"
+  },
+  {
+    "title": "Zlib",
+    "description": "Node.js native zlib library for gzip, deflate, and inflate",
+    "usage": "const zlib = require('zlib'); const compressed = zlib.gzipSync('Hello World');",
+    "tags": [
+      "zlib",
+      "gzip",
+      "deflate",
+      "compression"
+    ],
+    "category": "Utility"
   }
 ];
