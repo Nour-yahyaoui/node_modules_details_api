f57e81fc2cd0c7d4ea14026d1dfe82792d720730
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:30:09 +0200

    Add: Ncp to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 150
-// Last updated: 2026-08-25 08:30:07
+// Total modules: 151
+// Last updated: 2026-08-25 08:30:09
 
 module.exports = [
   {
@@ -1801,6 +1801,18 @@ module.exports = [
       "watch",
       "fs",
       "events"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Ncp",
+    "description": "Copy files and directories recursively with options for overwrite, filtering, and preserving permissions",
+    "usage": "const ncp = require('ncp'); ncp('src', 'dist', { stopOnErr: true }, err => { if (err) console.error(err); });",
+    "tags": [
+      "file",
+      "copy",
+      "recursive",
+      "fs"
     ],
     "category": "Utility"
   }
