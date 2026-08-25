d15b9547c5c440683a2ea58b29614836eedebfc3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:35:23 +0200

    Add: Fastify Sanitize to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 417
-// Last updated: 2026-08-25 08:35:22
+// Total modules: 418
+// Last updated: 2026-08-25 08:35:23
 
 module.exports = [
   {
@@ -5007,5 +5007,17 @@ module.exports = [
       "unicode"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Fastify Sanitize",
+    "description": "Input sanitization plugin for Fastify with XSS and SQL injection protection",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-sanitize'), { escapeHtml: true });",
+    "tags": [
+      "fastify",
+      "sanitize",
+      "security",
+      "xss"
+    ],
+    "category": "Security"
   }
 ];
