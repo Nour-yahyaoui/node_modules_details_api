0c831bdbabdd06cd7a8f2c8a731f2e25e9e3bbf8
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:30:24 +0200

    Add: Snowpack to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 163
-// Last updated: 2026-08-25 08:30:23
+// Total modules: 164
+// Last updated: 2026-08-25 08:30:24
 
 module.exports = [
   {
@@ -1957,6 +1957,18 @@ module.exports = [
       "tree-shaking",
       "esm",
       "library"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Snowpack",
+    "description": "Build tool using native ESM with fast builds, Hot Module Replacement, and no bundling",
+    "usage": "snowpack dev",
+    "tags": [
+      "build",
+      "esm",
+      "fast",
+      "hmr"
     ],
     "category": "Dev Tool"
   }
