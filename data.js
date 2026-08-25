b7a796fdc64e16a8e41c0e806ed1f9b51ca10158
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:30:37 +0200

    Add: Heroku CLI to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 174
-// Last updated: 2026-08-25 08:30:35
+// Total modules: 175
+// Last updated: 2026-08-25 08:30:36
 
 module.exports = [
   {
@@ -2086,6 +2086,18 @@ module.exports = [
     "usage": "netlify deploy --prod",
     "tags": [
       "netlify",
+      "deployment",
+      "hosting",
+      "cli"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Heroku CLI",
+    "description": "CLI for Heroku platform with app management, add-ons, and dyno scaling",
+    "usage": "heroku create my-app",
+    "tags": [
+      "heroku",
       "deployment",
       "hosting",
       "cli"
