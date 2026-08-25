7f42a953d8810ae42edfa55509e2c78a139dc76c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:31:40 +0200

    Add: Fastify Sensible to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 228
-// Last updated: 2026-08-25 08:31:38
+// Total modules: 229
+// Last updated: 2026-08-25 08:31:40
 
 module.exports = [
   {
@@ -2739,5 +2739,17 @@ module.exports = [
       "middleware"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Fastify Sensible",
+    "description": "Collection of useful Fastify plugins including defaults, assertions, and utilities",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/sensible'));",
+    "tags": [
+      "fastify",
+      "utilities",
+      "assert",
+      "middleware"
+    ],
+    "category": "Utility"
   }
 ];
