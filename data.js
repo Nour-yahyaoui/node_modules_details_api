8110b515555744cc869294af1626c7a791934f87
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:28:27 +0200

    Add: Forever to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 63
-// Last updated: 2026-08-25 08:28:26
+// Total modules: 64
+// Last updated: 2026-08-25 08:28:27
 
 module.exports = [
   {
@@ -757,6 +757,18 @@ module.exports = [
       "production",
       "manager",
       "monitoring"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Forever",
+    "description": "Simple daemon for keeping Node.js processes running continuously with automatic restarts on failure",
+    "usage": "forever start app.js",
+    "tags": [
+      "process",
+      "daemon",
+      "restart",
+      "persistent"
     ],
     "category": "Dev Tool"
   }
