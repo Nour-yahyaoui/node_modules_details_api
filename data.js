2d7875bc244998e8befff9632d992e91d40a94e2
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:35:48 +0200

    Add: Fastify AB Testing to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 438
-// Last updated: 2026-08-25 08:35:46
+// Total modules: 439
+// Last updated: 2026-08-25 08:35:47
 
 module.exports = [
   {
@@ -5257,6 +5257,18 @@ module.exports = [
       "feature-flags",
       "toggle",
       "rollout"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify AB Testing",
+    "description": "A/B testing plugin for Fastify with experiment allocation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-ab-testing'), { experiments: { newUI: { ratio: 0.5 } } });",
+    "tags": [
+      "fastify",
+      "ab-testing",
+      "experiments",
+      "allocation"
     ],
     "category": "Utility"
   }
