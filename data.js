f08ecbb4eba5bfe20bb5f53219e312dc4cb76f41
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:31:04 +0200

    Add: Meilisearch to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 197
-// Last updated: 2026-08-25 08:31:03
+// Total modules: 198
+// Last updated: 2026-08-25 08:31:04
 
 module.exports = [
   {
@@ -2365,6 +2365,18 @@ module.exports = [
       "elasticsearch",
       "analytics",
       "full-text"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "Meilisearch",
+    "description": "Fast search engine with typo-tolerance, filtering, and instant search capabilities",
+    "usage": "const { MeiliSearch } = require('meilisearch'); const client = new MeiliSearch({ host: 'http://localhost:7700' }); const results = await client.index('movies').search('Batman');",
+    "tags": [
+      "search",
+      "meilisearch",
+      "full-text",
+      "typo-tolerance"
     ],
     "category": "Database"
   }
