7fe16fb28435255cb4472d6f9a34a06b94992eaa
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:35:39 +0200

    Add: Fastify TOML to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 431
-// Last updated: 2026-08-25 08:35:38
+// Total modules: 432
+// Last updated: 2026-08-25 08:35:39
 
 module.exports = [
   {
@@ -5173,6 +5173,18 @@ module.exports = [
       "yaml",
       "parse",
       "schema"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify TOML",
+    "description": "TOML parsing and generation plugin for Fastify",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-toml'), { });",
+    "tags": [
+      "fastify",
+      "toml",
+      "parse",
+      "generate"
     ],
     "category": "Utility"
   }
