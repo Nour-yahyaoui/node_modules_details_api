184501148d90bb2073ee80b166a6d1a81b01eb02
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:36:50 +0200

    Add: Fastify EventBridge to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 490
-// Last updated: 2026-08-25 08:36:49
+// Total modules: 491
+// Last updated: 2026-08-25 08:36:50
 
 module.exports = [
   {
@@ -5881,6 +5881,18 @@ module.exports = [
       "step-functions",
       "aws",
       "workflow"
+    ],
+    "category": "Cloud"
+  },
+  {
+    "title": "Fastify EventBridge",
+    "description": "AWS EventBridge plugin for Fastify with event publishing",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-eventbridge'), { region: 'us-east-1' });",
+    "tags": [
+      "fastify",
+      "eventbridge",
+      "aws",
+      "events"
     ],
     "category": "Cloud"
   }
