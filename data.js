ab21169954eab02030065320318546fb605cc962
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:35:57 +0200

    Add: Fastify QR to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 446
-// Last updated: 2026-08-25 08:35:56
+// Total modules: 447
+// Last updated: 2026-08-25 08:35:57
 
 module.exports = [
   {
@@ -5353,6 +5353,18 @@ module.exports = [
       "image",
       "sharp",
       "processing"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify QR",
+    "description": "QR code generation and scanning plugin for Fastify",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-qr'), { });",
+    "tags": [
+      "fastify",
+      "qr",
+      "generate",
+      "scan"
     ],
     "category": "Utility"
   }
