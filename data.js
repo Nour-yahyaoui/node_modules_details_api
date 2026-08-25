99164dc4833edbfd4089033e76a193c20fffb148
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:32:31 +0200

    Add: TypeORM Subscribers to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 272
-// Last updated: 2026-08-25 08:32:30
+// Total modules: 273
+// Last updated: 2026-08-25 08:32:31
 
 module.exports = [
   {
@@ -3265,6 +3265,18 @@ module.exports = [
       "migrations",
       "database",
       "schema"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "TypeORM Subscribers",
+    "description": "TypeORM event subscribers with beforeInsert, beforeUpdate, and afterLoad",
+    "usage": "import { EventSubscriber, EntitySubscriberInterface } from 'typeorm'; @EventSubscriber() export class UserSubscriber implements EntitySubscriberInterface<User> { listenTo() { return User; } beforeInsert(event) { event.entity.createdAt = new Date(); } }",
+    "tags": [
+      "typeorm",
+      "subscribers",
+      "events",
+      "hooks"
     ],
     "category": "ORM/Database"
   }
