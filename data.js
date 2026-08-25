f8575db4fcdd9e44e5faef45cc71c83f579caeda
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-08-25 08:36:21 +0200

    Add: Fastify Material to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 466
-// Last updated: 2026-08-25 08:36:19
+// Total modules: 467
+// Last updated: 2026-08-25 08:36:20
 
 module.exports = [
   {
@@ -5592,6 +5592,18 @@ module.exports = [
       "fastify",
       "daisyui",
       "tailwind",
+      "components"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Material",
+    "description": "Material Design integration plugin for Fastify with components",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-material'), { });",
+    "tags": [
+      "fastify",
+      "material",
+      "design",
       "components"
     ],
     "category": "Utility"
