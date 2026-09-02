6bf0f060ab0e0042c23486774d95c4db3570dd3b
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:03:57 +0200

    Add: Fastify Archive to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 715
-// Last updated: 2026-09-02 20:03:56
+// Total modules: 716
+// Last updated: 2026-09-02 20:03:57
 
 module.exports = [
   {
@@ -8581,6 +8581,18 @@ module.exports = [
       "library",
       "catalogs",
       "circulation"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Archive",
+    "description": "Archive plugin for Fastify with document storage and retrieval",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-archive'), { });",
+    "tags": [
+      "fastify",
+      "archive",
+      "storage",
+      "retrieval"
     ],
     "category": "Utility"
   }
