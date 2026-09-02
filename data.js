153fee8989fa8697e6d53f20d3412a46387e1b70
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:21:37 +0200

    Add: MikroORM Unit of Work to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 273
-// Last updated: 2026-09-02 15:21:36
+// Total modules: 274
+// Last updated: 2026-09-02 15:21:37
 
 module.exports = [
   {
@@ -3277,6 +3277,18 @@ module.exports = [
       "subscribers",
       "events",
       "hooks"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "MikroORM Unit of Work",
+    "description": "MikroORM unit of work with identity map and transaction management",
+    "usage": "import { MikroORM } from '@mikro-orm/core'; const orm = await MikroORM.init({ entities: [User], dbName: 'db', type: 'postgresql' }); const user = orm.em.create(User, { name: 'John' }); await orm.em.persistAndFlush(user);",
+    "tags": [
+      "mikroorm",
+      "unit-of-work",
+      "identity-map",
+      "transaction"
     ],
     "category": "ORM/Database"
   }
