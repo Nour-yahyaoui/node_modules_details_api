25a34fdfc4ec5f2652f2f84dc48ef5cfc6d9c2d5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:21:09 +0200

    Add: Prisma Accelerate to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 250
-// Last updated: 2026-09-02 15:21:07
+// Total modules: 251
+// Last updated: 2026-09-02 15:21:09
 
 module.exports = [
   {
@@ -3003,5 +3003,17 @@ module.exports = [
       "typescript"
     ],
     "category": "ORM/Database"
+  },
+  {
+    "title": "Prisma Accelerate",
+    "description": "Prisma extension for connection pooling and edge caching",
+    "usage": "import { PrismaClient } from '@prisma/client'; import { withAccelerate } from '@prisma/extension-accelerate'; const prisma = new PrismaClient().$extends(withAccelerate());",
+    "tags": [
+      "prisma",
+      "accelerate",
+      "cache",
+      "pooling"
+    ],
+    "category": "Performance"
   }
 ];
