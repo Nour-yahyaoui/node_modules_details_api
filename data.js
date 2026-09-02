e588b7b23df9088521a8c8b147c1388376719aa3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:01:07 +0200

    Add: Fastify Worker to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 577
-// Last updated: 2026-09-02 20:01:05
+// Total modules: 578
+// Last updated: 2026-09-02 20:01:07
 
 module.exports = [
   {
@@ -6927,5 +6927,17 @@ module.exports = [
       "processing"
     ],
     "category": "Background Processing"
+  },
+  {
+    "title": "Fastify Worker",
+    "description": "Worker thread management plugin for Fastify with task delegation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-worker'), { poolSize: 4 });",
+    "tags": [
+      "fastify",
+      "worker",
+      "threads",
+      "delegation"
+    ],
+    "category": "Performance"
   }
 ];
