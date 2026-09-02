2795517b7e93bf1cbff982529118439e9758a736
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:30 +0200

    Add: Fastify Markdown to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 402
-// Last updated: 2026-09-02 19:57:28
+// Total modules: 403
+// Last updated: 2026-09-02 19:57:30
 
 module.exports = [
   {
@@ -4825,6 +4825,18 @@ module.exports = [
       "handlebars",
       "template",
       "views"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Markdown",
+    "description": "Markdown rendering plugin for Fastify with custom plugins",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-markdown'), { });",
+    "tags": [
+      "fastify",
+      "markdown",
+      "render",
+      "html"
     ],
     "category": "Utility"
   }
