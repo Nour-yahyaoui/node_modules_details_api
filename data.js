7d5a089c48f4e81655f90492f2e7c96ad536b812
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:24:11 +0200

    Add: Fastify Middleware to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 397
-// Last updated: 2026-09-02 15:24:09
+// Total modules: 398
+// Last updated: 2026-09-02 15:24:11
 
 module.exports = [
   {
@@ -4767,5 +4767,17 @@ module.exports = [
       "transports"
     ],
     "category": "Logging"
+  },
+  {
+    "title": "Fastify Middleware",
+    "description": "Connect/Express middleware compatibility plugin for Fastify",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/middleware')); fastify.use(require('cors')());",
+    "tags": [
+      "fastify",
+      "middleware",
+      "compatibility",
+      "express"
+    ],
+    "category": "Web Framework"
   }
 ];
