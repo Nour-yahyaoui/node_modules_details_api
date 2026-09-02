5467a3e381c378cc8566db2ed075edf7aab4e7d6
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:55:37 +0200

    Add: Keygrip to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 309
-// Last updated: 2026-09-02 19:55:35
+// Total modules: 310
+// Last updated: 2026-09-02 19:55:37
 
 module.exports = [
   {
@@ -3709,6 +3709,18 @@ module.exports = [
       "random",
       "secure",
       "bytes"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "Keygrip",
+    "description": "Key rotation and signing for HMAC and cookie signatures",
+    "usage": "const Keygrip = require('keygrip'); const keys = new Keygrip(['secret1', 'secret2']); const sig = keys.sign('data');",
+    "tags": [
+      "keygrip",
+      "hmac",
+      "signature",
+      "cookie"
     ],
     "category": "Security"
   }
