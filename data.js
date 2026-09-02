1503b0997eac44bcf87442173a75d28332f1aeb3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:23:50 +0200

    Add: Fastify Trace to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 380
-// Last updated: 2026-09-02 15:23:49
+// Total modules: 381
+// Last updated: 2026-09-02 15:23:50
 
 module.exports = [
   {
@@ -4560,6 +4560,18 @@ module.exports = [
       "fastify",
       "metrics",
       "prometheus",
+      "monitoring"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Fastify Trace",
+    "description": "Distributed tracing plugin for Fastify with OpenTelemetry support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-trace'), { serviceName: 'my-service' });",
+    "tags": [
+      "fastify",
+      "trace",
+      "opentelemetry",
       "monitoring"
     ],
     "category": "Dev Tool"
