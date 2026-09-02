03e2c7e78bd0fbc838cdb47221f590ed315d67a4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:27 +0200

    Add: Fastify Pug to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 400
-// Last updated: 2026-09-02 19:57:26
+// Total modules: 401
+// Last updated: 2026-09-02 19:57:27
 
 module.exports = [
   {
@@ -4799,6 +4799,18 @@ module.exports = [
     "tags": [
       "fastify",
       "ejs",
+      "template",
+      "views"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Pug",
+    "description": "Pug template engine plugin for Fastify with compile and runtime support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/pug'), { root: './views' });",
+    "tags": [
+      "fastify",
+      "pug",
       "template",
       "views"
     ],
