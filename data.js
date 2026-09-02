dc6ab206258710dc4eaffdabe2c51936c13504e0
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:55:31 +0200

    Add: Stream Web to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 304
-// Last updated: 2026-09-02 19:55:29
+// Total modules: 305
+// Last updated: 2026-09-02 19:55:31
 
 module.exports = [
   {
@@ -3649,6 +3649,18 @@ module.exports = [
       "transform",
       "modify",
       "data"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Stream Web",
+    "description": "Web Streams API implementation for Node.js with WritableStream and ReadableStream",
+    "usage": "const { ReadableStream, WritableStream } = require('stream/web'); const stream = new ReadableStream({ start(controller) { controller.enqueue('Hello'); controller.close(); } });",
+    "tags": [
+      "stream",
+      "web",
+      "api",
+      "readable"
     ],
     "category": "Utility"
   }
