982e1071dbc30f0bb4a882471f760f7749f00550
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:31 +0200

    Add: Fastify Autoload (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 403
-// Last updated: 2026-09-02 19:57:30
+// Total modules: 404
+// Last updated: 2026-09-02 19:57:31
 
 module.exports = [
   {
@@ -4839,5 +4839,17 @@ module.exports = [
       "html"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Fastify Autoload (Advanced)",
+    "description": "Advanced plugin autoloader with hierarchical loading and options",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/autoload'), { dir: './plugins', options: { prefix: 'api' } });",
+    "tags": [
+      "fastify",
+      "autoload",
+      "plugins",
+      "configuration"
+    ],
+    "category": "Dev Tool"
   }
 ];
