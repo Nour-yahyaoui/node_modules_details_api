f7bf5d3cf9b390f27069799c34783dff3f231095
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:00:55 +0200

    Add: Fastify Screen Share to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 567
-// Last updated: 2026-09-02 20:00:53
+// Total modules: 568
+// Last updated: 2026-09-02 20:00:54
 
 module.exports = [
   {
@@ -6804,6 +6804,18 @@ module.exports = [
       "fastify",
       "audio",
       "chat",
+      "webrtc"
+    ],
+    "category": "Real-time"
+  },
+  {
+    "title": "Fastify Screen Share",
+    "description": "Screen sharing plugin for Fastify with WebRTC support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-screen-share'), { });",
+    "tags": [
+      "fastify",
+      "screen",
+      "share",
       "webrtc"
     ],
     "category": "Real-time"
