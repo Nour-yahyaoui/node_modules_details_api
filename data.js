62e18016028e275343ca85375c17b090ce6bfc9a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:02:28 +0200

    Add: Fastify Consumer to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 643
-// Last updated: 2026-09-02 20:02:27
+// Total modules: 644
+// Last updated: 2026-09-02 20:02:28
 
 module.exports = [
   {
@@ -7719,5 +7719,17 @@ module.exports = [
       "bootstrap"
     ],
     "category": "Web Framework"
+  },
+  {
+    "title": "Fastify Consumer",
+    "description": "Message consumer for Fastify with event processing",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-consumer'), { topics: ['events'] });",
+    "tags": [
+      "fastify",
+      "consumer",
+      "message",
+      "event"
+    ],
+    "category": "Real-time"
   }
 ];
