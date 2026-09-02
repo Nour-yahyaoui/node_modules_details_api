be7e259f78d073a6a3d91a943f03b9e40686e4c4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 21:41:04 +0200

    Add: Google Cloud Client to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 73
-// Last updated: 2026-09-02 21:40:59
+// Total modules: 74
+// Last updated: 2026-09-02 21:41:03
 
 module.exports = [
   {
@@ -877,6 +877,18 @@ module.exports = [
       "s3",
       "dynamodb",
       "lambda"
+    ],
+    "category": "Cloud"
+  },
+  {
+    "title": "Google Cloud Client",
+    "description": "Official Google Cloud client for Node.js with support for Storage, Firestore, Pub/Sub, and other GCP services",
+    "usage": "const { Storage } = require('@google-cloud/storage'); const storage = new Storage(); const [buckets] = await storage.getBuckets();",
+    "tags": [
+      "google-cloud",
+      "gcp",
+      "storage",
+      "firestore"
     ],
     "category": "Cloud"
   }
