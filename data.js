9fdc04bc7c371c1c6e4ffc9ef08dc422448aaf78
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:58:06 +0200

    Add: Fastify INI (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 432
-// Last updated: 2026-09-02 19:58:05
+// Total modules: 433
+// Last updated: 2026-09-02 19:58:06
 
 module.exports = [
   {
@@ -5185,6 +5185,18 @@ module.exports = [
       "toml",
       "parse",
       "generate"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify INI (Advanced)",
+    "description": "Advanced INI configuration plugin with sections and inheritance",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-ini'), { });",
+    "tags": [
+      "fastify",
+      "ini",
+      "configuration",
+      "parse"
     ],
     "category": "Utility"
   }
