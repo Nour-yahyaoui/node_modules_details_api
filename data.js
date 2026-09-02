39a9e2f90c288088630d62a1c7e35b87a4d31478
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:24:28 +0200

    Add: Fastify Bcrypt to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 410
-// Last updated: 2026-09-02 15:24:27
+// Total modules: 411
+// Last updated: 2026-09-02 15:24:28
 
 module.exports = [
   {
@@ -4923,5 +4923,17 @@ module.exports = [
       "png"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Fastify Bcrypt",
+    "description": "Bcrypt hashing plugin for Fastify with salt rounds and compare",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-bcrypt'), { saltRounds: 10 });",
+    "tags": [
+      "fastify",
+      "bcrypt",
+      "hash",
+      "security"
+    ],
+    "category": "Security"
   }
 ];
