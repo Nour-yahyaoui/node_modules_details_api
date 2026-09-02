fe419ef4caff712a9c6c0de5255eeb9b536cb1de
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:26:23 +0200

    Add: Fastify Elasticsearch (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 503
-// Last updated: 2026-09-02 15:26:22
+// Total modules: 504
+// Last updated: 2026-09-02 15:26:23
 
 module.exports = [
   {
@@ -6037,6 +6037,18 @@ module.exports = [
       "opensearch",
       "search",
       "analytics"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "Fastify Elasticsearch (Advanced)",
+    "description": "Advanced Elasticsearch plugin with aggregation and search features",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-elasticsearch'), { node: 'http://localhost:9200' });",
+    "tags": [
+      "fastify",
+      "elasticsearch",
+      "search",
+      "aggregation"
     ],
     "category": "Database"
   }
