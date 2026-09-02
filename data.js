83cbc16c29ade8c777dbd27a5645f4623eb3dd01
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:53:20 +0200

    Add: RxJS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 201
-// Last updated: 2026-09-02 19:53:19
+// Total modules: 202
+// Last updated: 2026-09-02 19:53:20
 
 module.exports = [
   {
@@ -2415,5 +2415,17 @@ module.exports = [
       "full-text"
     ],
     "category": "Database"
+  },
+  {
+    "title": "RxJS",
+    "description": "Reactive extensions library for async programming with observables, operators, and subscriptions",
+    "usage": "const { Observable } = require('rxjs'); const obs = new Observable(subscriber => { subscriber.next('Hello'); subscriber.complete(); }); obs.subscribe(value => console.log(value));",
+    "tags": [
+      "reactive",
+      "observables",
+      "async",
+      "streams"
+    ],
+    "category": "Utility"
   }
 ];
