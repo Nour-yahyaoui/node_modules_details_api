d1cc2457e18d1b651eab0f5a90c63df8f6dd591c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:22:31 +0200

    Add: HSTS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 316
-// Last updated: 2026-09-02 15:22:29
+// Total modules: 317
+// Last updated: 2026-09-02 15:22:31
 
 module.exports = [
   {
@@ -3793,6 +3793,18 @@ module.exports = [
       "security",
       "headers",
       "privacy"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "HSTS",
+    "description": "HTTP Strict Transport Security middleware for Express",
+    "usage": "const hsts = require('hsts'); app.use(hsts({ maxAge: 31536000 }));",
+    "tags": [
+      "hsts",
+      "security",
+      "ssl",
+      "headers"
     ],
     "category": "Security"
   }
