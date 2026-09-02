2863fd3f39c77e4e8a704353e08b1cfd8c2410be
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:00 +0200

    Add: Fastify Worker Threads to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 378
-// Last updated: 2026-09-02 19:56:59
+// Total modules: 379
+// Last updated: 2026-09-02 19:57:00
 
 module.exports = [
   {
@@ -4539,5 +4539,17 @@ module.exports = [
       "jobs"
     ],
     "category": "Background Processing"
+  },
+  {
+    "title": "Fastify Worker Threads",
+    "description": "Worker thread pool plugin for Fastify with CPU-intensive task offloading",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-worker-threads'), { poolSize: 4 });",
+    "tags": [
+      "fastify",
+      "worker",
+      "threads",
+      "performance"
+    ],
+    "category": "Performance"
   }
 ];
