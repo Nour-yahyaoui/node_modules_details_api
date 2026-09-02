9301e2c04a252f40717c4a7b2d5984c816b51f2c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:56:56 +0200

    Add: Fastify Etag to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 374
-// Last updated: 2026-09-02 19:56:54
+// Total modules: 375
+// Last updated: 2026-09-02 19:56:55
 
 module.exports = [
   {
@@ -4489,6 +4489,18 @@ module.exports = [
       "compression",
       "gzip",
       "brotli"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Fastify Etag",
+    "description": "ETag generation plugin for Fastify with cache validation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/etag'));",
+    "tags": [
+      "fastify",
+      "etag",
+      "cache",
+      "validation"
     ],
     "category": "Performance"
   }
