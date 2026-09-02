fbc042e97cff32c831e8d0e2cfd6914054e4c01c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:58:17 +0200

    Add: Fastify Notifications to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 441
-// Last updated: 2026-09-02 19:58:16
+// Total modules: 442
+// Last updated: 2026-09-02 19:58:17
 
 module.exports = [
   {
@@ -5295,5 +5295,17 @@ module.exports = [
       "tracing"
     ],
     "category": "Dev Tool"
+  },
+  {
+    "title": "Fastify Notifications",
+    "description": "Push notification plugin for Fastify with WebPush and Firebase",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-notifications'), { providers: ['webpush', 'firebase'] });",
+    "tags": [
+      "fastify",
+      "notifications",
+      "push",
+      "webpush"
+    ],
+    "category": "Utility"
   }
 ];
