86b2647b4c2564e6a77f8e82082e90aa89694f8a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:29 +0200

    Add: Fastify Handlebars to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 401
-// Last updated: 2026-09-02 19:57:27
+// Total modules: 402
+// Last updated: 2026-09-02 19:57:28
 
 module.exports = [
   {
@@ -4811,6 +4811,18 @@ module.exports = [
     "tags": [
       "fastify",
       "pug",
+      "template",
+      "views"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Handlebars",
+    "description": "Handlebars template engine plugin for Fastify with helpers",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/handlebars'), { root: './views' });",
+    "tags": [
+      "fastify",
+      "handlebars",
       "template",
       "views"
     ],
