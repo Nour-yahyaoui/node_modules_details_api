e97b157bad8493718960e125bea1abe80200b2b9
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:57:42 +0200

    Add: Fastify Crypto to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 412
-// Last updated: 2026-09-02 19:57:40
+// Total modules: 413
+// Last updated: 2026-09-02 19:57:42
 
 module.exports = [
   {
@@ -4945,6 +4945,18 @@ module.exports = [
       "argon2",
       "hash",
       "security"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "Fastify Crypto",
+    "description": "Crypto utilities plugin for Fastify with AES and random generation",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-crypto'), { algorithm: 'aes-256-cbc' });",
+    "tags": [
+      "fastify",
+      "crypto",
+      "encryption",
+      "random"
     ],
     "category": "Security"
   }
