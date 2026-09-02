321250d13317eec7d4984d57b98a8710cc24308e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:04:38 +0200

    Add: Fastify Quiz to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 747
-// Last updated: 2026-09-02 20:04:37
+// Total modules: 748
+// Last updated: 2026-09-02 20:04:38
 
 module.exports = [
   {
@@ -8965,6 +8965,18 @@ module.exports = [
       "questionnaire",
       "conditional",
       "logic"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Quiz",
+    "description": "Quiz plugin for Fastify with questions and scoring",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-quiz'), { });",
+    "tags": [
+      "fastify",
+      "quiz",
+      "questions",
+      "scoring"
     ],
     "category": "Utility"
   }
