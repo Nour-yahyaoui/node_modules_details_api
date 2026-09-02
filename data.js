f4760f043d326d387ab800acd3634f1bf9132456
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:02:59 +0200

    Add: Fastify Cinema to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 668
-// Last updated: 2026-09-02 20:02:57
+// Total modules: 669
+// Last updated: 2026-09-02 20:02:59
 
 module.exports = [
   {
@@ -8019,5 +8019,17 @@ module.exports = [
       "schedules"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Fastify Cinema",
+    "description": "Movie booking plugin for Fastify with showtimes and seats",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-cinema'), { });",
+    "tags": [
+      "fastify",
+      "cinema",
+      "movie",
+      "booking"
+    ],
+    "category": "Web Framework"
   }
 ];
