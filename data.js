4a3286a19b06e6bbedf743654dab85a4334ae97f
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:04:26 +0200

    Add: Fastify Collaboration to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 737
-// Last updated: 2026-09-02 20:04:24
+// Total modules: 738
+// Last updated: 2026-09-02 20:04:26
 
 module.exports = [
   {
@@ -8845,6 +8845,18 @@ module.exports = [
       "consensus",
       "agreement",
       "protocols"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Collaboration",
+    "description": "Collaboration plugin for Fastify with team workspaces",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-collaboration'), { });",
+    "tags": [
+      "fastify",
+      "collaboration",
+      "team",
+      "workspaces"
     ],
     "category": "Utility"
   }
