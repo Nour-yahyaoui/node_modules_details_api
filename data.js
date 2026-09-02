429096178954fe47b0b15b662ca49b50f84ec9d5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:55:03 +0200

    Add: Redis Cluster to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 281
-// Last updated: 2026-09-02 19:55:01
+// Total modules: 282
+// Last updated: 2026-09-02 19:55:02
 
 module.exports = [
   {
@@ -3373,6 +3373,18 @@ module.exports = [
       "prepared",
       "statements",
       "driver"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "Redis Cluster",
+    "description": "Redis cluster client with sharding, failover, and command routing",
+    "usage": "const Redis = require('ioredis'); const cluster = new Redis.Cluster([{ host: 'localhost', port: 6379 }]); await cluster.set('key', 'value');",
+    "tags": [
+      "redis",
+      "cluster",
+      "sharding",
+      "failover"
     ],
     "category": "Database"
   }
