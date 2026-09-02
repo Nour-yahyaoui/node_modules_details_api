4bce2d1c6202a5156946950fd312209ac94d09e4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:22:47 +0200

    Add: Response Time to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 329
-// Last updated: 2026-09-02 15:22:45
+// Total modules: 330
+// Last updated: 2026-09-02 15:22:47
 
 module.exports = [
   {
@@ -3951,5 +3951,17 @@ module.exports = [
       "middleware"
     ],
     "category": "Utility"
+  },
+  {
+    "title": "Response Time",
+    "description": "Response time header middleware for Express with timing metrics",
+    "usage": "const responseTime = require('response-time'); app.use(responseTime());",
+    "tags": [
+      "response-time",
+      "performance",
+      "metrics",
+      "express"
+    ],
+    "category": "Performance"
   }
 ];
