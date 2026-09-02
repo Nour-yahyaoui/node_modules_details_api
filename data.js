49f977a86da8b4eb8e272d42f3c94eda534fd962
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 20:05:01 +0200

    Add: Fastify Personalization to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 765
-// Last updated: 2026-09-02 20:04:59
+// Total modules: 766
+// Last updated: 2026-09-02 20:05:00
 
 module.exports = [
   {
@@ -9181,6 +9181,18 @@ module.exports = [
       "recommendation",
       "engine",
       "filtering"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Personalization",
+    "description": "Personalization plugin for Fastify with user preferences",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-personalization'), { });",
+    "tags": [
+      "fastify",
+      "personalization",
+      "preferences",
+      "user"
     ],
     "category": "Utility"
   }
