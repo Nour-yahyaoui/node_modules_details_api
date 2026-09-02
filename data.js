979a3047ed5d621dfc811913d094e8211e19a541
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 15:22:44 +0200

    Add: Connect Busboy to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 327
-// Last updated: 2026-09-02 15:22:43
+// Total modules: 328
+// Last updated: 2026-09-02 15:22:44
 
 module.exports = [
   {
@@ -3925,6 +3925,18 @@ module.exports = [
       "upload",
       "express",
       "middleware"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Connect Busboy",
+    "description": "Busboy middleware for streaming multipart file uploads",
+    "usage": "const busboy = require('connect-busboy'); app.use(busboy({ immediate: true }));",
+    "tags": [
+      "busboy",
+      "upload",
+      "stream",
+      "express"
     ],
     "category": "Utility"
   }
