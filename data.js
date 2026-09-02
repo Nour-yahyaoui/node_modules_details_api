8429cbf7b2691e8847a488cf2c1563d46cefa7ac
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-02 19:58:33 +0200

    Add: Fastify JSX to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 454
-// Last updated: 2026-09-02 19:58:32
+// Total modules: 455
+// Last updated: 2026-09-02 19:58:33
 
 module.exports = [
   {
@@ -5451,5 +5451,17 @@ module.exports = [
       "compilation"
     ],
     "category": "Dev Tool"
+  },
+  {
+    "title": "Fastify JSX",
+    "description": "JSX rendering plugin for Fastify with React support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-jsx'), { });",
+    "tags": [
+      "fastify",
+      "jsx",
+      "react",
+      "render"
+    ],
+    "category": "Utility"
   }
 ];
