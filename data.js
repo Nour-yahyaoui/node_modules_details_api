0198759cb023f1751d7b5db8019cfdf8192d5bc4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:14:12 +0200

    Add: Object Path to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 346
-// Last updated: 2026-09-05 17:14:10
+// Total modules: 347
+// Last updated: 2026-09-05 17:14:11
 
 module.exports = [
   {
@@ -4153,6 +4153,18 @@ module.exports = [
       "merge",
       "objects",
       "recursive"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Object Path",
+    "description": "Get, set, and delete nested object properties using path strings",
+    "usage": "const objectPath = require('object-path'); const obj = { user: { name: 'John' } }; objectPath.get(obj, 'user.name');",
+    "tags": [
+      "object",
+      "path",
+      "get",
+      "set"
     ],
     "category": "Utility"
   }
