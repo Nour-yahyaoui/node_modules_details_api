9d1fa4eb83ee13da9d4baede73be3e874719b9a8
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:33 +0200

    Add: Fastify Svelte to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 458
-// Last updated: 2026-09-05 17:16:31
+// Total modules: 459
+// Last updated: 2026-09-05 17:16:32
 
 module.exports = [
   {
@@ -5495,6 +5495,18 @@ module.exports = [
     "tags": [
       "fastify",
       "angular",
+      "ssr",
+      "render"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Svelte",
+    "description": "Svelte rendering plugin for Fastify with SSR support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-svelte'), { ssr: true });",
+    "tags": [
+      "fastify",
+      "svelte",
       "ssr",
       "render"
     ],
