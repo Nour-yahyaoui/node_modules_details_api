a070934d4610f0c1f24986826be1408ba70203d2
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:21:17 +0200

    Add: Fastify Weather to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 676
-// Last updated: 2026-09-05 17:21:15
+// Total modules: 677
+// Last updated: 2026-09-05 17:21:17
 
 module.exports = [
   {
@@ -8113,6 +8113,18 @@ module.exports = [
       "farm",
       "crops",
       "livestock"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Weather",
+    "description": "Weather plugin for Fastify with forecasts and alerts",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-weather'), { });",
+    "tags": [
+      "fastify",
+      "weather",
+      "forecasts",
+      "alerts"
     ],
     "category": "Utility"
   }
