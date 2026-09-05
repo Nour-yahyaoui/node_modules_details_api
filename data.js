fb9372d7de92128f69530c64b237ab61acca1dfd
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:20:02 +0200

    Add: Fastify Theme to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 618
-// Last updated: 2026-09-05 17:20:01
+// Total modules: 619
+// Last updated: 2026-09-05 17:20:02
 
 module.exports = [
   {
@@ -7417,6 +7417,18 @@ module.exports = [
       "widget",
       "dashboard",
       "manager"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Theme",
+    "description": "Theme system for Fastify with customizable UI themes",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-theme'), { themes: ['./themes'] });",
+    "tags": [
+      "fastify",
+      "theme",
+      "ui",
+      "customization"
     ],
     "category": "Utility"
   }
