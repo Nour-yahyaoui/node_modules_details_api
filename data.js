4d4ab1ce9f55b1f28767b86939585d415c8569b4
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:04:48 +0200

    Add: Cli-spinners to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 209
-// Last updated: 2026-09-05 17:04:46
+// Total modules: 210
+// Last updated: 2026-09-05 17:04:48
 
 module.exports = [
   {
@@ -2509,6 +2509,18 @@ module.exports = [
       "colors",
       "styling",
       "ansi"
+    ],
+    "category": "CLI/Tool"
+  },
+  {
+    "title": "Cli-spinners",
+    "description": "Collection of terminal spinners for CLI applications with customizable styles",
+    "usage": "const cliSpinners = require('cli-spinners'); console.log(cliSpinners.dots);",
+    "tags": [
+      "cli",
+      "spinner",
+      "terminal",
+      "animation"
     ],
     "category": "CLI/Tool"
   }
