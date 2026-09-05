f58fc14b8e5b59a93927a7db8c944e502251ee3d
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:29 +0200

    Add: Fastify Vue to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 455
-// Last updated: 2026-09-05 17:16:27
+// Total modules: 456
+// Last updated: 2026-09-05 17:16:29
 
 module.exports = [
   {
@@ -5460,6 +5460,18 @@ module.exports = [
       "fastify",
       "jsx",
       "react",
+      "render"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Vue",
+    "description": "Vue.js rendering plugin for Fastify with SSR support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-vue'), { ssr: true });",
+    "tags": [
+      "fastify",
+      "vue",
+      "ssr",
       "render"
     ],
     "category": "Utility"
