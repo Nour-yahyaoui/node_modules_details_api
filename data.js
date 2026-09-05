526bd12afe0a35e8fe7021208a04fa1a7a4ce10a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:15:41 +0200

    Add: Fastify Normalize to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 416
-// Last updated: 2026-09-05 17:15:39
+// Total modules: 417
+// Last updated: 2026-09-05 17:15:40
 
 module.exports = [
   {
@@ -4993,6 +4993,18 @@ module.exports = [
       "slug",
       "generate",
       "url"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Normalize",
+    "description": "String normalization plugin for Fastify with unicode and case handling",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-normalize'), { case: 'lower' });",
+    "tags": [
+      "fastify",
+      "normalize",
+      "string",
+      "unicode"
     ],
     "category": "Utility"
   }
