fc3c9dfa0f7a5058da4c8e3c0d408ef8f958307e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:19:23 +0200

    Add: Fastify WebDAV to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 588
-// Last updated: 2026-09-05 17:19:21
+// Total modules: 589
+// Last updated: 2026-09-05 17:19:22
 
 module.exports = [
   {
@@ -7057,6 +7057,18 @@ module.exports = [
       "sftp",
       "secure",
       "transfer"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify WebDAV",
+    "description": "WebDAV plugin for Fastify with file management and sharing",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-webdav'), { root: './files' });",
+    "tags": [
+      "fastify",
+      "webdav",
+      "files",
+      "sharing"
     ],
     "category": "Utility"
   }
