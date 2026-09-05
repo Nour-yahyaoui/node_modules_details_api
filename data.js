05d3703156e45f621c885ec5d691c2970e6013fc
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:21:23 +0200

    Add: Fastify Fleet to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 681
-// Last updated: 2026-09-05 17:21:22
+// Total modules: 682
+// Last updated: 2026-09-05 17:21:23
 
 module.exports = [
   {
@@ -8173,6 +8173,18 @@ module.exports = [
       "navigation",
       "directions",
       "turn-by-turn"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Fleet",
+    "description": "Fleet management plugin for Fastify with vehicles and tracking",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-fleet'), { });",
+    "tags": [
+      "fastify",
+      "fleet",
+      "vehicles",
+      "tracking"
     ],
     "category": "Utility"
   }
