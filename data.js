d77f2ef6f17b3569aa7b5694f16983554639d795
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:15:37 +0200

    Add: Fastify UUID to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 413
-// Last updated: 2026-09-05 17:15:35
+// Total modules: 414
+// Last updated: 2026-09-05 17:15:37
 
 module.exports = [
   {
@@ -4959,5 +4959,17 @@ module.exports = [
       "random"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Fastify UUID",
+    "description": "UUID generation plugin for Fastify with v1, v4, and v5 support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-uuid'), { version: 'v4' });",
+    "tags": [
+      "fastify",
+      "uuid",
+      "generate",
+      "identifier"
+    ],
+    "category": "Utility"
   }
 ];
