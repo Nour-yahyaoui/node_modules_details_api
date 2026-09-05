7b880da5e3296fcfb885ec564c811700f530c72a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:40:42 +0200

    Add: Fastify Project to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 739
-// Last updated: 2026-09-05 17:22:37
+// Total modules: 740
+// Last updated: 2026-09-05 17:22:39
 
 module.exports = [
   {
@@ -8869,6 +8869,18 @@ module.exports = [
       "workspace",
       "projects",
       "management"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Project",
+    "description": "Project management plugin for Fastify with milestones and tasks",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-project'), { });",
+    "tags": [
+      "fastify",
+      "project",
+      "milestones",
+      "tasks"
     ],
     "category": "Utility"
   }
