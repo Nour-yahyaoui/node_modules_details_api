e1d308d31cf0769c2c07fd68bf856b9b797dd09c
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:18:17 +0200

    Add: Fastify Path Aliases to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 538
-// Last updated: 2026-09-05 17:18:16
+// Total modules: 539
+// Last updated: 2026-09-05 17:18:17
 
 module.exports = [
   {
@@ -6457,6 +6457,18 @@ module.exports = [
       "aliases",
       "module",
       "resolution"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "Fastify Path Aliases",
+    "description": "Path alias plugin for Fastify with import mapping",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-path-aliases'), { paths: { '~': './src' } });",
+    "tags": [
+      "fastify",
+      "path",
+      "aliases",
+      "import"
     ],
     "category": "Dev Tool"
   }
