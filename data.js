a833aeef152b7f93b197732e5d759483e1293bcc
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:13:00 +0200

    Add: Fast LRU to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 293
-// Last updated: 2026-09-05 17:12:58
+// Total modules: 294
+// Last updated: 2026-09-05 17:13:00
 
 module.exports = [
   {
@@ -3517,6 +3517,18 @@ module.exports = [
       "cache",
       "performance",
       "function"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Fast LRU",
+    "description": "Fast LRU cache implementation with Map-based storage and TTL",
+    "usage": "const lru = require('fast-lru'); const cache = new lru({ max: 100, ttl: 60000 }); cache.set('key', 'value');",
+    "tags": [
+      "lru",
+      "cache",
+      "fast",
+      "ttl"
     ],
     "category": "Performance"
   }
