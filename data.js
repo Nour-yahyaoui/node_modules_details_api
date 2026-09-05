2f013a6efd92ec9d95793ffc3ead89b6737191ba
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:14:46 +0200

    Add: Fastify Compress (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 373
-// Last updated: 2026-09-05 17:14:44
+// Total modules: 374
+// Last updated: 2026-09-05 17:14:46
 
 module.exports = [
   {
@@ -4477,6 +4477,18 @@ module.exports = [
       "cache",
       "etag",
       "performance"
+    ],
+    "category": "Performance"
+  },
+  {
+    "title": "Fastify Compress (Advanced)",
+    "description": "Advanced compression plugin with custom algorithms and thresholds",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/compress'), { threshold: 2048, customTypes: ['text/custom'] });",
+    "tags": [
+      "fastify",
+      "compression",
+      "gzip",
+      "brotli"
     ],
     "category": "Performance"
   }
