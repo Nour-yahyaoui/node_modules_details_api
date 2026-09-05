dd51742e94a6a5e07c529cea1e580214cf077457
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:22:04 +0200

    Add: Fastify University to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 713
-// Last updated: 2026-09-05 17:22:03
+// Total modules: 714
+// Last updated: 2026-09-05 17:22:04
 
 module.exports = [
   {
@@ -8557,6 +8557,18 @@ module.exports = [
       "school",
       "classes",
       "schedules"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify University",
+    "description": "University plugin for Fastify with departments and faculties",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-university'), { });",
+    "tags": [
+      "fastify",
+      "university",
+      "departments",
+      "faculties"
     ],
     "category": "Utility"
   }
