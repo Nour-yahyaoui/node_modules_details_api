40cd43b24ef5633eb95382c899efbb9558abdcf5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 15:21:22 +0200

    Add: Luxon to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 106
-// Last updated: 2026-09-05 15:21:20
+// Total modules: 107
+// Last updated: 2026-09-05 15:21:22
 
 module.exports = [
   {
@@ -1273,6 +1273,18 @@ module.exports = [
       "time",
       "datetime",
       "format"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Luxon",
+    "description": "Powerful date and time library with immutable objects, durations, and interval support",
+    "usage": "const { DateTime } = require('luxon'); const now = DateTime.now(); const formatted = now.toFormat('yyyy-MM-dd HH:mm:ss');",
+    "tags": [
+      "date",
+      "time",
+      "datetime",
+      "immutable"
     ],
     "category": "Utility"
   }
