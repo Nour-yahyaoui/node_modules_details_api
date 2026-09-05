8e6739987be951cd48f4e43657a9208a9fa5cd5a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:19:17 +0200

    Add: Fastify Terminal to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 584
-// Last updated: 2026-09-05 17:19:16
+// Total modules: 585
+// Last updated: 2026-09-05 17:19:17
 
 module.exports = [
   {
@@ -7009,6 +7009,18 @@ module.exports = [
       "shell",
       "commands",
       "exec"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Terminal",
+    "description": "Terminal emulation plugin for Fastify with web-based terminal",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-terminal'), { });",
+    "tags": [
+      "fastify",
+      "terminal",
+      "web",
+      "emulation"
     ],
     "category": "Utility"
   }
