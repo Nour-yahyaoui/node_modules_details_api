d3507adaf4552ab97c1f9874d44f749f3cb79fd1
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:03:07 +0200

    Add: Glob to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 148
-// Last updated: 2026-09-05 17:03:05
+// Total modules: 149
+// Last updated: 2026-09-05 17:03:07
 
 module.exports = [
   {
@@ -1777,6 +1777,18 @@ module.exports = [
       "commands",
       "cross-platform",
       "unix"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Glob",
+    "description": "Pattern matching for files using glob syntax with sync and async matching options",
+    "usage": "const glob = require('glob'); const files = glob.sync('src/**/*.js'); console.log(files);",
+    "tags": [
+      "files",
+      "pattern",
+      "matching",
+      "glob"
     ],
     "category": "Utility"
   }
