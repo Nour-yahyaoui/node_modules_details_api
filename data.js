816e675901c58621e01069e66f8e5c81cac6b558
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:05:51 +0200

    Add: Knex Migrations to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 254
-// Last updated: 2026-09-05 17:05:50
+// Total modules: 255
+// Last updated: 2026-09-05 17:05:51
 
 module.exports = [
   {
@@ -3051,5 +3051,17 @@ module.exports = [
       "introspection"
     ],
     "category": "Dev Tool"
+  },
+  {
+    "title": "Knex Migrations",
+    "description": "Database migration management for Knex with up/down and seed support",
+    "usage": "const knex = require('knex')({ client: 'pg' }); await knex.schema.createTable('users', (table) => { table.increments('id'); table.string('name'); });",
+    "tags": [
+      "knex",
+      "migrations",
+      "sql",
+      "database"
+    ],
+    "category": "ORM/Database"
   }
 ];
