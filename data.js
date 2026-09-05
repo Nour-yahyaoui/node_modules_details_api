74fe38c337c5ac1d199cfddddedf0f9ff3562dd7
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:20:00 +0200

    Add: Fastify Component to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 616
-// Last updated: 2026-09-05 17:19:58
+// Total modules: 617
+// Last updated: 2026-09-05 17:19:59
 
 module.exports = [
   {
@@ -7395,5 +7395,17 @@ module.exports = [
       "dependency"
     ],
     "category": "Dev Tool"
+  },
+  {
+    "title": "Fastify Component",
+    "description": "Component system for Fastify with reusable components",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-component'), { components: ['./components'] });",
+    "tags": [
+      "fastify",
+      "component",
+      "reusable",
+      "system"
+    ],
+    "category": "Web Framework"
   }
 ];
