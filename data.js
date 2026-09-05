04640208462161d8d9e040219a5e83459dae2cd9
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:13:41 +0200

    Add: Serve Favicon to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 322
-// Last updated: 2026-09-05 17:13:39
+// Total modules: 323
+// Last updated: 2026-09-05 17:13:41
 
 module.exports = [
   {
@@ -3867,5 +3867,17 @@ module.exports = [
       "express"
     ],
     "category": "Database"
+  },
+  {
+    "title": "Serve Favicon",
+    "description": "Favicon serving middleware for Express with caching",
+    "usage": "const favicon = require('serve-favicon'); app.use(favicon('favicon.ico'));",
+    "tags": [
+      "favicon",
+      "express",
+      "static",
+      "serve"
+    ],
+    "category": "Utility"
   }
 ];
