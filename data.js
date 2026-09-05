4fb5b71c97c6bea5db8d20708d618f24ca59cd50
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:18:25 +0200

    Add: Fastify Transformer to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 544
-// Last updated: 2026-09-05 17:18:23
+// Total modules: 545
+// Last updated: 2026-09-05 17:18:25
 
 module.exports = [
   {
@@ -6531,5 +6531,17 @@ module.exports = [
       "rules"
     ],
     "category": "Validation"
+  },
+  {
+    "title": "Fastify Transformer",
+    "description": "Data transformation plugin for Fastify with serialization",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-transformer'), { transformers: { user: (data) => ({ ...data, fullName: data.name }) } });",
+    "tags": [
+      "fastify",
+      "transformer",
+      "serialization",
+      "data"
+    ],
+    "category": "Utility"
   }
 ];
