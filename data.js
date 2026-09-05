4eed58974aa5a220ae71af1543a96f486a2789f3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:21:42 +0200

    Add: Fastify Retail to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 695
-// Last updated: 2026-09-05 17:21:40
+// Total modules: 696
+// Last updated: 2026-09-05 17:21:41
 
 module.exports = [
   {
@@ -8341,6 +8341,18 @@ module.exports = [
       "grocery",
       "shopping",
       "delivery"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Retail",
+    "description": "Retail plugin for Fastify with POS and inventory",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-retail'), { });",
+    "tags": [
+      "fastify",
+      "retail",
+      "pos",
+      "inventory"
     ],
     "category": "Utility"
   }
