dacc23e07dce8e73e72d20b000628c7b20ee1ac3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:22:02 +0200

    Add: Fastify Education to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 711
-// Last updated: 2026-09-05 17:22:00
+// Total modules: 712
+// Last updated: 2026-09-05 17:22:01
 
 module.exports = [
   {
@@ -8533,6 +8533,18 @@ module.exports = [
       "learning",
       "courses",
       "lessons"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Education",
+    "description": "Education plugin for Fastify with students and grades",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-education'), { });",
+    "tags": [
+      "fastify",
+      "education",
+      "students",
+      "grades"
     ],
     "category": "Utility"
   }
