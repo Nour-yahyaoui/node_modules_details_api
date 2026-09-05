9a32aad637793a5d84d0455e3e8ecf774371ec91
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 15:21:50 +0200

    Add: GraphQL-WS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 128
-// Last updated: 2026-09-05 15:21:49
+// Total modules: 129
+// Last updated: 2026-09-05 15:21:50
 
 module.exports = [
   {
@@ -1539,5 +1539,17 @@ module.exports = [
       "presence"
     ],
     "category": "Real-time"
+  },
+  {
+    "title": "GraphQL-WS",
+    "description": "GraphQL over WebSocket protocol implementation for real-time subscriptions with GraphQL",
+    "usage": "const { useServer } = require('graphql-ws/lib/use/ws'); const server = useServer({ schema }, wsServer);",
+    "tags": [
+      "graphql",
+      "websocket",
+      "subscriptions",
+      "realtime"
+    ],
+    "category": "API"
   }
 ];
