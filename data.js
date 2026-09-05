0207ee69ac7fedfb33e4b07cd0ffe8bec0431212
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:20:01 +0200

    Add: Fastify Widget to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 617
-// Last updated: 2026-09-05 17:19:59
+// Total modules: 618
+// Last updated: 2026-09-05 17:20:01
 
 module.exports = [
   {
@@ -7407,5 +7407,17 @@ module.exports = [
       "system"
     ],
     "category": "Web Framework"
+  },
+  {
+    "title": "Fastify Widget",
+    "description": "Widget manager for Fastify with dashboard widgets",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-widget'), { widgets: ['./widgets'] });",
+    "tags": [
+      "fastify",
+      "widget",
+      "dashboard",
+      "manager"
+    ],
+    "category": "Utility"
   }
 ];
