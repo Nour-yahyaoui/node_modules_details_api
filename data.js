fc98541f4bf313ef6f902a9e1fa6f664aefa0b2a
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:15:12 +0200

    Add: Fastify ACL to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 393
-// Last updated: 2026-09-05 17:15:10
+// Total modules: 394
+// Last updated: 2026-09-05 17:15:12
 
 module.exports = [
   {
@@ -4717,6 +4717,18 @@ module.exports = [
       "rbac",
       "roles",
       "permissions"
+    ],
+    "category": "Security"
+  },
+  {
+    "title": "Fastify ACL",
+    "description": "Access control list plugin for Fastify with fine-grained permissions",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-acl'), { allow: ['admin', 'user'] });",
+    "tags": [
+      "fastify",
+      "acl",
+      "access-control",
+      "security"
     ],
     "category": "Security"
   }
