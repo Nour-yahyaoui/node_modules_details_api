8b85a7bdf1bb8a2db491e4840a30aca1d8565cd3
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:06:11 +0200

    Add: TypeORM Relations to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 270
-// Last updated: 2026-09-05 17:06:10
+// Total modules: 271
+// Last updated: 2026-09-05 17:06:11
 
 module.exports = [
   {
@@ -3240,6 +3240,18 @@ module.exports = [
       "sequelize",
       "seeders",
       "data",
+      "database"
+    ],
+    "category": "ORM/Database"
+  },
+  {
+    "title": "TypeORM Relations",
+    "description": "TypeORM entity relations with one-to-one, one-to-many, and many-to-many",
+    "usage": "import { Entity, OneToMany, ManyToOne } from 'typeorm'; @Entity() class User { @OneToMany(() => Post, post => post.user) posts: Post[]; } @Entity() class Post { @ManyToOne(() => User, user => user.posts) user: User; }",
+    "tags": [
+      "typeorm",
+      "relations",
+      "entities",
       "database"
     ],
     "category": "ORM/Database"
