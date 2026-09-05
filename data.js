60b2e5d1cca4fb31ca76d536d28c8151b74dd731
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:20:41 +0200

    Add: Fastify Observer to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 649
-// Last updated: 2026-09-05 17:20:40
+// Total modules: 650
+// Last updated: 2026-09-05 17:20:41
 
 module.exports = [
   {
@@ -7789,6 +7789,18 @@ module.exports = [
       "listener",
       "event",
       "handling"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Observer",
+    "description": "Observer pattern plugin for Fastify with state change notifications",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-observer'), { subjects: ['./subjects'] });",
+    "tags": [
+      "fastify",
+      "observer",
+      "pattern",
+      "notifications"
     ],
     "category": "Utility"
   }
