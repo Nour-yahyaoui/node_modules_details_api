869505e144442b3fb0921db335876db063db68ec
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:13 +0200

    Add: Fastify SMS to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 442
-// Last updated: 2026-09-05 17:16:11
+// Total modules: 443
+// Last updated: 2026-09-05 17:16:13
 
 module.exports = [
   {
@@ -5305,6 +5305,18 @@ module.exports = [
       "notifications",
       "push",
       "webpush"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify SMS",
+    "description": "SMS sending plugin for Fastify with Twilio and Vonage integration",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-sms'), { provider: 'twilio' });",
+    "tags": [
+      "fastify",
+      "sms",
+      "twilio",
+      "vonage"
     ],
     "category": "Utility"
   }
