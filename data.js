f75400852d0b7cda1c18af1571be97fa788b5ec0
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:15:32 +0200

    Add: Fastify QR Code to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 409
-// Last updated: 2026-09-05 17:15:31
+// Total modules: 410
+// Last updated: 2026-09-05 17:15:32
 
 module.exports = [
   {
@@ -4911,5 +4911,17 @@ module.exports = [
       "security"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Fastify QR Code",
+    "description": "QR code generation plugin for Fastify with PNG and SVG output",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-qrcode'), { type: 'png' });",
+    "tags": [
+      "fastify",
+      "qrcode",
+      "generate",
+      "png"
+    ],
+    "category": "Utility"
   }
 ];
