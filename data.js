ee27087913f7112844f70f85dc27d4036a77f7ec
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:46 +0200

    Add: Fastify Icons to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 468
-// Last updated: 2026-09-05 17:16:44
+// Total modules: 469
+// Last updated: 2026-09-05 17:16:46
 
 module.exports = [
   {
@@ -5617,6 +5617,18 @@ module.exports = [
       "font-awesome",
       "icons",
       "font"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Icons",
+    "description": "Icon library integration plugin for Fastify with multiple providers",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-icons'), { provider: 'material' });",
+    "tags": [
+      "fastify",
+      "icons",
+      "material",
+      "font-awesome"
     ],
     "category": "Utility"
   }
