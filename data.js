767cd2e23a1823316910574a7c46db13392b87d1
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:21 +0200

    Add: Fastify Word to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 449
-// Last updated: 2026-09-05 17:16:20
+// Total modules: 450
+// Last updated: 2026-09-05 17:16:21
 
 module.exports = [
   {
@@ -5389,6 +5389,18 @@ module.exports = [
       "exceljs",
       "excel",
       "charts"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Word",
+    "description": "Word document generation plugin with DOCX support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-word'), { });",
+    "tags": [
+      "fastify",
+      "word",
+      "docx",
+      "generate"
     ],
     "category": "Utility"
   }
