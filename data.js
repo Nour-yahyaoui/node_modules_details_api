faca41e735eddd2163de6a5a4149f5c94701a236
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:19:31 +0200

    Add: Fastify Snapshot (Advanced) to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 595
-// Last updated: 2026-09-05 17:19:30
+// Total modules: 596
+// Last updated: 2026-09-05 17:19:31
 
 module.exports = [
   {
@@ -7141,6 +7141,18 @@ module.exports = [
       "restore",
       "recovery",
       "point-in-time"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Snapshot (Advanced)",
+    "description": "Advanced snapshot plugin with versioning and rollback",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-snapshot'), { versions: 5 });",
+    "tags": [
+      "fastify",
+      "snapshot",
+      "versioning",
+      "rollback"
     ],
     "category": "Utility"
   }
