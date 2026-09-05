208c65a40122b3f95403e0d8896391d631a35994
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:03:22 +0200

    Add: TypeScript to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 159
-// Last updated: 2026-09-05 17:03:21
+// Total modules: 160
+// Last updated: 2026-09-05 17:03:22
 
 module.exports = [
   {
@@ -1909,6 +1909,18 @@ module.exports = [
       "babel",
       "es6",
       "compiler"
+    ],
+    "category": "Dev Tool"
+  },
+  {
+    "title": "TypeScript",
+    "description": "Typed superset of JavaScript that compiles to plain JavaScript with full type checking",
+    "usage": "npx tsc app.ts",
+    "tags": [
+      "typescript",
+      "typing",
+      "compiler",
+      "language"
     ],
     "category": "Dev Tool"
   }
