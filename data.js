3c7a029ac97ea7e6eff5efcb1549501810dcdb8d
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:15:38 +0200

    Add: Fastify Nanoid to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 414
-// Last updated: 2026-09-05 17:15:37
+// Total modules: 415
+// Last updated: 2026-09-05 17:15:38
 
 module.exports = [
   {
@@ -4969,6 +4969,18 @@ module.exports = [
       "uuid",
       "generate",
       "identifier"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Nanoid",
+    "description": "NanoID generation plugin for Fastify with custom alphabet",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-nanoid'), { length: 10 });",
+    "tags": [
+      "fastify",
+      "nanoid",
+      "id",
+      "generate"
     ],
     "category": "Utility"
   }
