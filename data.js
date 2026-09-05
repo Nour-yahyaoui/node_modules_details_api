768f9307704ee8dd61556f74c41e81b6261607c8
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:21:03 +0200

    Add: Fastify Podcast to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 665
-// Last updated: 2026-09-05 17:21:01
+// Total modules: 666
+// Last updated: 2026-09-05 17:21:02
 
 module.exports = [
   {
@@ -7981,6 +7981,18 @@ module.exports = [
       "music",
       "midi",
       "notation"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Podcast",
+    "description": "Podcast plugin for Fastify with RSS and episode management",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-podcast'), { });",
+    "tags": [
+      "fastify",
+      "podcast",
+      "rss",
+      "episodes"
     ],
     "category": "Utility"
   }
