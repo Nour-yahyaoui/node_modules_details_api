67409081c4843534717adc708de4d6e0bcdf74a1
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:24 +0200

    Add: Fastify HTML to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 451
-// Last updated: 2026-09-05 17:16:22
+// Total modules: 452
+// Last updated: 2026-09-05 17:16:24
 
 module.exports = [
   {
@@ -5413,6 +5413,18 @@ module.exports = [
       "ppt",
       "powerpoint",
       "presentation"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify HTML",
+    "description": "HTML generation and template plugin for Fastify",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-html'), { });",
+    "tags": [
+      "fastify",
+      "html",
+      "template",
+      "generate"
     ],
     "category": "Utility"
   }
