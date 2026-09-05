00fb1343bc6611cb56e2876f5f3b878f410d76d7
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:12:49 +0200

    Add: MongoDB Aggregation Expressions to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 285
-// Last updated: 2026-09-05 17:12:48
+// Total modules: 286
+// Last updated: 2026-09-05 17:12:49
 
 module.exports = [
   {
@@ -3421,6 +3421,18 @@ module.exports = [
       "transactions",
       "acid",
       "session"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "MongoDB Aggregation Expressions",
+    "description": "MongoDB aggregation with $addFields, $project, and conditional operators",
+    "usage": "const results = await db.collection('users').aggregate([ { $addFields: { fullName: { $concat: ['$firstName', ' ', '$lastName'] } } }, { $project: { name: '$fullName', age: 1 } } ]).toArray();",
+    "tags": [
+      "mongodb",
+      "aggregation",
+      "expressions",
+      "pipeline"
     ],
     "category": "Database"
   }
