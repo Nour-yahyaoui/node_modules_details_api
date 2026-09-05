4494cc4a793dfc7645a3240261b22dff83cb6c37
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:05:15 +0200

    Add: Fastify Swagger to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 226
-// Last updated: 2026-09-05 17:05:14
+// Total modules: 227
+// Last updated: 2026-09-05 17:05:15
 
 module.exports = [
   {
@@ -2715,5 +2715,17 @@ module.exports = [
       "payments"
     ],
     "category": "Payment"
+  },
+  {
+    "title": "Fastify Swagger",
+    "description": "OpenAPI documentation generator for Fastify with interactive Swagger UI",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/swagger'));",
+    "tags": [
+      "fastify",
+      "swagger",
+      "openapi",
+      "documentation"
+    ],
+    "category": "Dev Tool"
   }
 ];
