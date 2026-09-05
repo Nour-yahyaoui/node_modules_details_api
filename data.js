63305c2fe7dfe3ec4541004cf0a4d96fedaf87a8
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:21:38 +0200

    Add: Fastify Dining to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 692
-// Last updated: 2026-09-05 17:21:36
+// Total modules: 693
+// Last updated: 2026-09-05 17:21:38
 
 module.exports = [
   {
@@ -8305,6 +8305,18 @@ module.exports = [
       "hospitality",
       "hotels",
       "restaurants"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Dining",
+    "description": "Dining plugin for Fastify with reservations and menus",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-dining'), { });",
+    "tags": [
+      "fastify",
+      "dining",
+      "reservations",
+      "menus"
     ],
     "category": "Utility"
   }
