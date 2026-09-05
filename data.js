585fe9ef13b359e88d388b3779d2cd87d8a1215e
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 15:15:29 +0200

    Add: Prettier to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 25
-// Last updated: 2026-09-05 15:15:28
+// Total modules: 26
+// Last updated: 2026-09-05 15:15:29
 
 module.exports = [
   {
@@ -303,5 +303,17 @@ module.exports = [
       "login"
     ],
     "category": "Security"
+  },
+  {
+    "title": "Prettier",
+    "description": "Opinionated code formatter supporting JavaScript, TypeScript, CSS, JSON, and many other languages",
+    "usage": "npx prettier --write .",
+    "tags": [
+      "format",
+      "code-style",
+      "opinionated",
+      "automation"
+    ],
+    "category": "Dev Tool"
   }
 ];
