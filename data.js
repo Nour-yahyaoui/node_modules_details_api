b3f7eb636116bb3b8f2c98c6721fd79ebb66ac4b
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:16:35 +0200

    Add: Fastify Lit to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 460
-// Last updated: 2026-09-05 17:16:34
+// Total modules: 461
+// Last updated: 2026-09-05 17:16:35
 
 module.exports = [
   {
@@ -5520,6 +5520,18 @@ module.exports = [
       "fastify",
       "solid",
       "ssr",
+      "render"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Lit",
+    "description": "Lit HTML rendering plugin for Fastify with web components",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-lit'), { });",
+    "tags": [
+      "fastify",
+      "lit",
+      "web-components",
       "render"
     ],
     "category": "Utility"
