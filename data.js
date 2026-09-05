0a3b94a9a6b3c3b5c19e93f8d8007b7d36c9494d
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:17:32 +0200

    Add: Fastify Meilisearch to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 504
-// Last updated: 2026-09-05 17:17:31
+// Total modules: 505
+// Last updated: 2026-09-05 17:17:32
 
 module.exports = [
   {
@@ -6049,6 +6049,18 @@ module.exports = [
       "elasticsearch",
       "search",
       "aggregation"
+    ],
+    "category": "Database"
+  },
+  {
+    "title": "Fastify Meilisearch",
+    "description": "Meilisearch plugin for Fastify with instant search and typo-tolerance",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-meilisearch'), { host: 'http://localhost:7700' });",
+    "tags": [
+      "fastify",
+      "meilisearch",
+      "search",
+      "typo-tolerance"
     ],
     "category": "Database"
   }
