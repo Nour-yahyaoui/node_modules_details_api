7050eb30687c6bc9b1b98c3460d7a0237c3558bf
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:14:29 +0200

    Add: Fastify GraphQL to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 359
-// Last updated: 2026-09-05 17:14:27
+// Total modules: 360
+// Last updated: 2026-09-05 17:14:28
 
 module.exports = [
   {
@@ -4311,5 +4311,17 @@ module.exports = [
       "realtime"
     ],
     "category": "Real-time"
+  },
+  {
+    "title": "Fastify GraphQL",
+    "description": "GraphQL plugin for Fastify with schema stitching and resolver support",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('@fastify/graphql'), { schema, resolvers });",
+    "tags": [
+      "fastify",
+      "graphql",
+      "api",
+      "schema"
+    ],
+    "category": "API"
   }
 ];
