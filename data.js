280128d7f181f8ad02244b30a253688b1a3505a5
nour-yahyaoui <nourryahyaoui@gmail.com>
2026-09-05 17:22:30 +0200

    Add: Fastify Poll to data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Node.js Library Documentation
-// Total modules: 733
-// Last updated: 2026-09-05 17:22:28
+// Total modules: 734
+// Last updated: 2026-09-05 17:22:30
 
 module.exports = [
   {
@@ -8797,6 +8797,18 @@ module.exports = [
       "rating",
       "stars",
       "scores"
+    ],
+    "category": "Utility"
+  },
+  {
+    "title": "Fastify Poll",
+    "description": "Poll plugin for Fastify with questions and votes",
+    "usage": "const fastify = require('fastify')(); fastify.register(require('fastify-poll'), { });",
+    "tags": [
+      "fastify",
+      "poll",
+      "questions",
+      "votes"
     ],
     "category": "Utility"
   }
